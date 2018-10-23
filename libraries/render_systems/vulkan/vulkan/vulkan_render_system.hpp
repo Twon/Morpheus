0a7ed91337502dcb190259381d2cbfbfddef5da3
Twon <ant.peacock@gmail.com>
2018-10-23 21:01:15 +0100

    Add projects for all render systems to be supported

---
+++ libraries/render_systems/vulkan/vulkan/vulkan_render_system.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <core/gfx/render_system.hpp>
+
+namespace morpheus::gfx::vulkan
+{
+
+/*! \class render_system
+ */
+class render_system {
+public:
+    virtual ~render_system();
+
+private:
+};
+
+} // namespace morpheus::gfx::vulkan
