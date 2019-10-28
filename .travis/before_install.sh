0a171f56cd053cf128de539edc0a3bfcb41d10a1
Antony Peacock <ant.peacock@gmail.com>
2019-10-28 15:30:19 +0000

    Add set up script for Python3 and virtual environments on Mac

---
+++ .travis/before_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    brew update
+    brew install openssl readline
+
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+
+    if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+    if which pyenv-virtualenv > /dev/null; then eval "$(pyenv-virtualenv init -)"; fi
+
+    pyenv install 3.7.0
+    pyenv virtualenv 3.7.0 conan
+
+    pyenv rehash
+    pyenv activate conan
+
+    python --version
+fi
