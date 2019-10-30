21e58f56fb65a25b28d526cc29dd86b456ed90d3
Antony Peacock <ant.peacock@gmail.com>
2019-10-30 11:39:52 +0000

    Enable unit testing

--- cmake/ThirdParty.cmake
+++ cmake/ThirdParty.cmake
@@ -8,6 +8,9 @@ FetchContent_GetProperties(catch2)
 if(NOT catch2_POPULATED)
     FetchContent_Populate(catch2)
     add_subdirectory(${catch2_SOURCE_DIR} ${catch2_BINARY_DIR})
+    list(APPEND CMAKE_MODULE_PATH
+        ${catch2_SOURCE_DIR}/contrib
+    )
 endif()
 
 #find_package(catch2)
