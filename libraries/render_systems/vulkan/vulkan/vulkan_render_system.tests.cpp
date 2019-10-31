15bf04b2ecaef7ec4ee062074abf75891a7921fa
Antony Peacock <ant.peacock@gmail.com>
2019-10-31 12:31:21 +0000

    Add logging to test

--- libraries/render_systems/vulkan/vulkan/vulkan_render_system.tests.cpp
+++ libraries/render_systems/vulkan/vulkan/vulkan_render_system.tests.cpp
@@ -17,16 +17,18 @@ TEST_CASE("Create a video mode list", "[morpheus.core.gfx.video_mode_list]")
 {
     uint32_t version = 0;
     VkResult res = vkEnumerateInstanceVersion(&version);
-    std::cout << "The Vulkan version is " << VK_VERSION_MAJOR(version) << "." << VK_VERSION_MINOR(version) << "." << VK_VERSION_PATCH(version);
+    INFO("The Vulkan version is " << VK_VERSION_MAJOR(version) <<"."<< VK_VERSION_MINOR(version) <<"."<< VK_VERSION_PATCH(version));
     
     uint32_t instance_layer_count = 0;
     res = vkEnumerateInstanceLayerProperties(&instance_layer_count, nullptr);
     
+    INFO("Found " << instance_layer_count << " layers in Vulkan instance.");
     std::vector<VkLayerProperties> layers(instance_layer_count);
     res = vkEnumerateInstanceLayerProperties(&instance_layer_count, layers.data());
 
     std::vector<char const *> layer_names(instance_layer_count);
     std::transform(layers.begin(), layers.end(), layer_names.begin(), [](const auto& element) { return element.layerName;});
+    std::for_each(layers.begin(), layers.end(),  [](const auto& element) { INFO("Layer: " << element.layerName);});
 
 /*    for (const auto& element : layers)
     {
