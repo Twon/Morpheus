20ddc98b6f4aab2c5bcdddbcea5c7279406bc75d
Antony Peacock <ant.peacock@gmail.com>
2022-02-26 09:19:24 +0000

    Specialisation utility

--- libraries/core/core/meta/is_specialisation.hpp
+++ libraries/core/core/meta/is_specialisation.hpp
@@ -1,17 +1,20 @@
 #include <type_traits>
 
-namespace morpheus
+namespace morpheus::meta
 {
 
 // https://stackoverflow.com/questions/11251376/how-can-i-check-if-a-type-is-an-instantiation-of-a-given-class-template#comment14786989_11251408
-template < template <typename...> class Template, typename T >
+template < class T >
 struct is_specialisation_of : std::false_type {};
 
 template < template <typename...> class Template, typename... Args >
-struct is_specialisation_of< Template, Template<Args...> > : std::true_type {};
+struct is_specialisation_of< Template<Args...> > : std::true_type {};
 
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/p2098r0.pdf
-template< class T, template<class...> class Primary >
-inline constexpr bool is_specialization_of_v = is_specialisation_of<T, Primary>::value;
+template< class T >
+inline constexpr bool is_specialisation_of_v = false;
 
-} // namespace morpheus
+template < template <typename...> class Template, typename... Args >
+inline constexpr bool is_specialisation_of_v< Template<Args...> > = true;
+
+} // namespace morpheus::meta
