77034a0035dde0565c58d6880e8678cbcd4c4f6f
Antony Peacock <ant.peacock@gmail.com>
2022-09-01 10:15:06 +0100

    Rule of zero for WGL context

--- libraries/render_systems/gl4/gl4/wgl/context.cpp
+++ libraries/render_systems/gl4/gl4/wgl/context.cpp
@@ -6,52 +6,43 @@ namespace morpheus::gfx::gl4::wgl
 {
 
 Context::Context(HWND const window, PIXELFORMATDESCRIPTOR const& pfd)
-:   mOwningWindow(window),
-    mDeviceContext([&]()
+:   mOwningWindow(window)
+,   mDeviceContext([&]()
         {
             MORPHEUS_ASSERT(window);
             auto const hDC = GetDC(window);
             MORPHEUS_WGL_VERIFY(hDC);
             return hDC;
         }()
+    ,   { [this]() { MORPHEUS_WGL_VERIFY(ReleaseDC(*mOwningWindow, mDeviceContext.get())); } }
     )
 ,   mGLContext([&]()
         {
-		    if (int format = ChoosePixelFormat(mDeviceContext, &pfd); format != 0)
+		    if (int format = ChoosePixelFormat(mDeviceContext.get(), &pfd); format != 0)
 		    {
 			    // ChoosePixelFormat calls out to graphics adapter drivers.  During this nvodl64v.dll set this even during success
 			    // and its not cleared at the end of the function call.
 			    SetLastError(0);
-			    SetPixelFormat(mDeviceContext, format, &pfd);
+			    SetPixelFormat(mDeviceContext.get(), format, &pfd);
 		    }
-            auto const hGLContext = wglCreateContext(mDeviceContext);
+            auto const hGLContext = wglCreateContext(mDeviceContext.get());
             MORPHEUS_WGL_VERIFY(hGLContext);
             return hGLContext;
         }()
+    ,   { [this]() { MORPHEUS_WGL_VERIFY(wglDeleteContext(mGLContext.get())); } }
     )
-,   mOwningContext(true)
 {
 }
 
 Context::Context()
-:   mDeviceContext(wglGetCurrentDC())
-,   mGLContext(wglGetCurrentContext())
-,   mOwningContext(false)
+:   mDeviceContext(wglGetCurrentDC(), {[](){}})
+,   mGLContext(wglGetCurrentContext(), {[](){}})
 {}
-
-Context::~Context()
-{
-    if (mOwningWindow)
-    {
-        wglDeleteContext(mGLContext);
-        MORPHEUS_WGL_VERIFY(ReleaseDC(*mOwningWindow, mDeviceContext));
-    }
-}
 
 Context Context::enable()
 {
     Context currentContext;
-    wglMakeCurrent(mDeviceContext, mGLContext);
+    MORPHEUS_WGL_VERIFY(wglMakeCurrent(mDeviceContext.get(), mGLContext.get()));
     return currentContext;
 }
 
