37c567e29a216a931b7b0cce937846c6890a8cf0
Antony Peacock <ant.peacock@gmail.com>
2022-09-29 20:15:04 +0100

    Concept and construction tests for write serialisers

--- libraries/core/core/serialisation/concepts/writer.hpp
+++ libraries/core/core/serialisation/concepts/writer.hpp
@@ -10,31 +10,31 @@
 namespace morpheus::serialisation::concepts
 {
 
-    /// \concept Writer
-    ///     Constraint for a type capabile of writing the fundamental types to an archive source.
-    template <typename T>
-    concept Writer = requires(T t)
-    {
-        { t.isTextual() } -> std::same_as<bool>;
+/// \concept Writer
+///     Constraint for a type capabile of writing the fundamental types to an archive source.
+template <typename T>
+concept Writer = requires(T t)
+{
+    { t.isTextual() } -> std::same_as<bool>;
 
-        { t.beginComposite() } -> std::same_as<void>;
-        { t.endComposite() } -> std::same_as<void>;
-        { t.beginValue(std::string_view{}) } -> std::same_as<void>;
-        { t.endValue() } -> std::same_as<void>;
+    { t.beginComposite() } -> std::same_as<void>;
+    { t.endComposite() } -> std::same_as<void>;
+    { t.beginValue(std::string_view{}) } -> std::same_as<void>;
+    { t.endValue() } -> std::same_as<void>;
 
-        { t.write(bool{}) } -> std::same_as<void>;
-        { t.write(std::uint8_t{}) } ->std::same_as<void>;
-        { t.write(std::int8_t{}) } -> std::same_as<void>;
-        { t.write(std::uint16_t{}) } -> std::same_as<void>;
-        { t.write(std::int16_t{}) } -> std::same_as<void>;
-        { t.write(std::uint32_t{}) } -> std::same_as<void>;
-        { t.write(std::int32_t{}) } -> std::same_as<void>;
-        { t.write(std::uint64_t{}) } -> std::same_as<void>;
-        { t.write(std::int64_t{}) } -> std::same_as<void>;
-        { t.write(float{}) } -> std::same_as<void>;
-        { t.write(double{}) } -> std::same_as<void>;
-        { t.write(std::string_view{}) } -> std::same_as<void>;
-        { t.write(std::span<std::byte>{}) } -> std::same_as<void>;
-    };
+    { t.write(bool{}) } -> std::same_as<void>;
+    { t.write(std::uint8_t{}) } ->std::same_as<void>;
+    { t.write(std::int8_t{}) } -> std::same_as<void>;
+    { t.write(std::uint16_t{}) } -> std::same_as<void>;
+    { t.write(std::int16_t{}) } -> std::same_as<void>;
+    { t.write(std::uint32_t{}) } -> std::same_as<void>;
+    { t.write(std::int32_t{}) } -> std::same_as<void>;
+    { t.write(std::uint64_t{}) } -> std::same_as<void>;
+    { t.write(std::int64_t{}) } -> std::same_as<void>;
+    { t.write(float{}) } -> std::same_as<void>;
+    { t.write(double{}) } -> std::same_as<void>;
+    { t.write(std::string_view{}) } -> std::same_as<void>;
+    { t.write(std::span<std::byte>{}) } -> std::same_as<void>;
+};
 
 } // morpheus::serialisation::concepts
--- libraries/core/core/serialisation/write_serialiser.tests.cpp
+++ libraries/core/core/serialisation/write_serialiser.tests.cpp
@@ -9,11 +9,111 @@ namespace morpheus::serialisation
 
 namespace {
 
-class TestWriter
+struct WriterArchtype
 {
+    static constexpr bool isTextual() { return false; }
 
+    void beginComposite() {}
+    void endComposite() {}
+    void beginValue(std::string_view const key) {}
+    void endValue() {}
+    void beginSequence(std::optional<std::size_t> = std::nullopt) {}
+    void endSequence() {}
+
+    template<typename T>
+    requires std::is_scalar_v<T> or 
+             std::is_floating_point_v<T> or 
+             std::is_same_v<T, std::string_view> or 
+             std::is_same_v<T, std::span<std::byte>>
+    void write(T const value) {}
 };
 
+}
+
+TEST_CASE("Check concepts for write serialisation", "[morpheus.serialisation.write_serialiser.concepts]")
+{
+    STATIC_REQUIRE(concepts::Writer<WriterArchtype>);
+}
+
+TEST_CASE("Ensure write serialiser are default constructible where the underlying writer supports it", "[morpheus.serialisation.write_serialiser.default_construction]")
+{
+    GIVEN("A default constructible writer")
+    {
+        STATIC_REQUIRE(std::is_default_constructible_v<WriterArchtype>);
+
+        WHEN("Ensure write serialiser of given tyoe")
+        {
+            using WriteSerialiserArchitype = WriteSerialiser<WriterArchtype>;
+            THEN("Is default construtible")
+            {
+                STATIC_REQUIRE(std::is_default_constructible_v<WriteSerialiserArchitype>);
+            }
+        }
+    }
+    GIVEN("A non-default constructible writer")
+    {
+        struct NonDefaultConstuctibleWriter : WriterArchtype
+        {
+            NonDefaultConstuctibleWriter() = delete;
+        };
+        STATIC_REQUIRE(!std::is_default_constructible_v<NonDefaultConstuctibleWriter>);
+
+        WHEN("Ensure write serialiser of given tyoe")
+        {
+            using NonDefaultConstuctibleWriteSerialiserArchitype = WriteSerialiser<NonDefaultConstuctibleWriter>;
+            THEN("Is default construtible")
+            {
+                STATIC_REQUIRE(!std::is_default_constructible_v<NonDefaultConstuctibleWriteSerialiserArchitype>);
+            }
+        }
+    }
+}
+
+TEST_CASE("Ensure write serialiser are forward constructible where the underlying writer supports it", "[morpheus.serialisation.write_serialiser.forwarding_construction]")
+{
+    GIVEN("A writer with multiple constructors")
+    {
+        struct MultiConstructorWriter : WriterArchtype
+        {
+            MultiConstructorWriter() = delete;
+            MultiConstructorWriter(int, bool) {}
+            MultiConstructorWriter(int, bool, int, bool);
+        };
+
+        STATIC_REQUIRE(std::is_constructible_v<MultiConstructorWriter, int, bool>);
+        STATIC_REQUIRE(std::is_constructible_v<MultiConstructorWriter, int, bool, int, bool>);
+
+        WHEN("Ensure write serialiser of given tyoe")
+        {
+            using ForwardingSerialiserArchitype = WriteSerialiser<MultiConstructorWriter>;
+            THEN("Is default construtible")
+            {
+                STATIC_REQUIRE(!std::is_constructible_v<ForwardingSerialiserArchitype, int>);
+                STATIC_REQUIRE(std::is_constructible_v<ForwardingSerialiserArchitype, int, bool>);
+                STATIC_REQUIRE(!std::is_constructible_v<ForwardingSerialiserArchitype, int, bool, int>);
+                STATIC_REQUIRE(std::is_constructible_v<ForwardingSerialiserArchitype, int, bool, int, bool>);
+            }
+        }
+    }
+    GIVEN("A non-default constructible writer")
+    {
+        STATIC_REQUIRE(!std::is_constructible_v<WriterArchtype, int>);
+        STATIC_REQUIRE(!std::is_constructible_v<WriterArchtype, int, bool>);
+        STATIC_REQUIRE(!std::is_constructible_v<WriterArchtype, int, bool, int>);
+        STATIC_REQUIRE(!std::is_constructible_v<WriterArchtype, int, bool, int, bool>);
+
+        WHEN("Ensure write serialiser of given tyoe")
+        {
+            using WriteSerialiserArchitype = WriteSerialiser<WriterArchtype>;
+            THEN("Is default construtible")
+            {
+                STATIC_REQUIRE(!std::is_constructible_v<WriteSerialiserArchitype, int>);
+                STATIC_REQUIRE(!std::is_constructible_v<WriteSerialiserArchitype, int, bool>);
+                STATIC_REQUIRE(!std::is_constructible_v<WriteSerialiserArchitype, int, bool, int>);
+                STATIC_REQUIRE(!std::is_constructible_v<WriteSerialiserArchitype, int, bool, int, bool>);
+            }
+        }
+    }
 }
 
 } // namespace morpheus::serialisation
