f45bef0c9127f9d93542c83caf139c37f8df1d29
Antony Peacock <ant.peacock@gmail.com>
2022-10-09 11:35:08 +0100

    Missing functional files

---
+++ libraries/core/core/functional/overload.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <utility>
+
+namespace morpheus::functional
+{
+
+/// \class Overload
+///     Starting point or an overload type build of lambdas
+/// \note
+///     Following the example of https://www.modernescpp.com/index.php/visiting-a-std-variant-with-the-overload-pattern
+template<typename... Ts>                                  
+struct Overload : Ts... {
+    using Ts::operator()...;
+};
+
+template<class... Ts> Overload(Ts...)->Overload<Ts...>;
+
+template <class... Ts>
+auto makeVisitor(Ts... ts)
+{
+    return Overload<Ts...>(std::forward<Ts>(ts)...);
+}
+
+}
