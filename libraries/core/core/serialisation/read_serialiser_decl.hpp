031fc6f9b97dd9e5f683da99bd623ec6e87b61f3
Antony Peacock <ant.peacock@gmail.com>
2022-10-18 10:29:03 +0100

    Document serialisation dispatch

--- libraries/core/core/serialisation/read_serialiser_decl.hpp
+++ libraries/core/core/serialisation/read_serialiser_decl.hpp
@@ -51,13 +51,26 @@ public:
     [[nodiscard]] ReaderType& reader() noexcept { return mReader; }
     [[nodiscard]] ReaderType const& reader() const noexcept { return mReader; }
 
+    /// \name Deserialise
+    ///     Custom deserialise function specialisations should deserialise sub-members via the writes serialisers
+    ///     methods.  This allows breaking of cirular dependencies in the dispatching of deserialisation calls and
+    ///     delays look up of the actual deserialisation call to the second phase of the two-phase lookup. With the
+    ///     dependency broken the actual underlying calls dispatch to the deserialise customisation point object which
+    ///     then finds the correct call via ADL dispatch.
+    ///@{
+    /// Deserialise a single value
+    /// \tparam T The underlying type of value to deserialise.
+    /// \return The deserialises value.
     template<typename T>
-//    requires std::is_arithmetic_v<T>
     [[nodiscard]] T deserialise();
 
+    /// Deserialise a key value pair
+    /// \tparam T The underlying type of value to deserialise.
+    /// \param[in] key The key to serialise.
+    /// \return The deserialises value.
     template<typename T>
     [[nodiscard]] T deserialise(std::string_view const key);
-
+    ///@}
 private:
     ReaderType mReader;
 };
--- libraries/core/core/serialisation/write_serialiser_decl.hpp
+++ libraries/core/core/serialisation/write_serialiser_decl.hpp
@@ -32,11 +32,26 @@ public:
     [[nodiscard]] WriterType const& writer() const noexcept { return mWriter; }
 #endif // (__cpp_explicit_this_parameter >= 202110)
 
+    /// \name Serialise
+    ///     Custom serialise function specialisations should serialise sub-members via the writes serialisers
+    ///     methods.  This allows breaking of cirular dependencies in the dispatching of serialisation calls and delays
+    ///     look up of the actual serialisation call to the second phase of the two-phase lookup. With the dependency
+    ///     broken the actual underlying calls dispatch to the serialise customisation point object which then finds
+    ///     the correct call via ADL dispatch.
+    ///@{
+    /// Serialise a single value
+    /// \tparam T The underlying type of value to serialise.
+    /// \param[in] value The value to serialise.
     template<typename T>
     void serialise(T const& value);
 
+    /// Serialise a key value pair
+    /// \tparam T The underlying type of value to serialise.
+    /// \param[in] key The key to serialise.
+    /// \param[in] value The value to serialise.
     template<typename T>
     void serialise(std::string_view const key, T const& value);
+    ///@}
 private:
     WriterType mWriter;
 };
