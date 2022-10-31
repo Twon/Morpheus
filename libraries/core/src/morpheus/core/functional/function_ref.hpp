9caea42cacb0d301e2cf2ef57ac01883d0791a82
Antony Peacock <ant.peacock@gmail.com>
2022-10-31 12:45:56 +0000

    Construct from free-functions

--- libraries/core/src/morpheus/core/functional/function_ref.hpp
+++ libraries/core/src/morpheus/core/functional/function_ref.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <morpheus/core/conformance/version.hpp>
+#include <morpheus/core/meta/invocable_traits.hpp>
 #include <concepts>
+#include <type_traits>
 
 #if (__cpp_lib_function_ref >= 202202L)
 
@@ -11,20 +13,59 @@ namespace morpheus { namespace func_ref_ns = std; }
 
 namespace morpheus::func_ref_ns 
 {
+
+/// \struct nontype_t
+///     nontype argument tag
+/// \note
+///     Proposed by p2472r3: make function_ref more functional
+template<auto V>
+struct nontype_t {
+    explicit nontype_t() = default;
+};
+
+template<auto V> inline constexpr nontype_t<V> nontype{};
+
 template<class Signature> class function_ref; 
 
 /// \class function_ref
 ///     Implementation of [p0792r10](https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2022/p0792r10.html) 
-///     function_ref, a simple non-owning reference to a callable.
-template<typename Return, class... ArgTypes>
-class function_ref<Return(ArgTypes...)> {
+///     function_ref, a simple non-owning reference to a callable.  This allows binding to callables of:
+///     - Lmabdas/Functors
+///     - Free functions
+///     - Member functions
+/// \note
+///      [func.wrap.ref.class]
+template<typename Return, class... Args>
+class function_ref<Return(Args...)> {
 public:
-    using signature_type = Return(*)(ArgTypes...);
+    using signature_type = Return(*)(Args...);
 
     constexpr function_ref() = delete;
 
-/*  
-    function_ref(const function_ref&) noexcept = default;
+    template<class F>
+    function_ref(F* f) noexcept requires std::is_function_v<F>//and is_invocable_using<F>
+    :   mInvoke( 
+            [](Storage storage, Args&&... args) noexcept(isNoexcept)
+            {
+                if constexpr (std::is_void_v<Return>)
+                    function_ref::get<F>(storage)(std::forward<Args>(args)...);
+                else
+                    return function_ref::get<F>(storage)(std::forward<Args>(args)...);
+            })
+    ,   mStorage(f)
+    {}
+
+    template<auto f> constexpr function_ref(nontype_t<f>) noexcept
+    {
+
+    }
+
+//    template<auto f, class T> constexpr function_ref(nontype_t<f>, T&) noexcept;
+//    template<auto f, class T> constexpr function_ref(nontype_t<f>, cv T*) noexcept;
+
+    function_ref(function_ref const&) noexcept = default;
+
+   /*  
 
     template <typename F>
     function_ref(F&&);
@@ -37,14 +78,70 @@ public:
     void swap(function_ref&) noexcept;
 
     R operator()(Args...) const noexcept(see below);*/
+
+    constexpr Return operator()(Args&&... args) const noexcept(isNoexcept)
+    {
+        return mInvoke(mStorage, std::forward<Args>(args)...);
+    }
+
+    constexpr void swap(function_ref& other) noexcept
+    {
+        std::swap(mInvoke, other.mInvoke);
+        std::swap(mStorage, other.mStorage);
+    }
+
 private:
+    static constexpr bool isNoexcept = meta::IsNothrowInvocable<Return, Args...>;
 
+    union Storage
+    {
+        void* p = nullptr;
+        void const* cp;
+        void (*fp)();
 
+        constexpr Storage() noexcept = default;
+
+        template<class T> requires std::is_object_v<T>
+        constexpr explicit Storage(T* t) noexcept : p(t)
+        {}
+
+        template<class T> requires std::is_object_v<T>
+        constexpr explicit Storage(T const* t) noexcept : cp(t)
+        {}
+
+        template<class F> requires std::is_function_v<F>
+        constexpr explicit Storage(F* f) noexcept : fp(f)
+        {}
+    };
+
+    template<typename T> requires std::is_object_v<T> or std::is_function_v<T>
+    static constexpr auto get(Storage const storage)
+    {
+        if constexpr (std::is_const_v<T>)
+            return static_cast<T const*>(storage.cp);
+        else if constexpr (std::is_object_v<T>)
+            return static_cast<T*>(storage.p);
+        else
+            return static_cast<T*>(storage.fp);
+    }
+
+    using InternalInvocableType = Return(Storage, Args...);
+
+    InternalInvocableType* mInvoke = nullptr;
+    Storage mStorage;
 };
 
+
 // [func.wrap.ref.deduct]
-template<class F>
+template<class F> requires std::is_function_v<F>
 function_ref(F*)->function_ref<F>;
+
+template<auto f> requires std::is_function_v<decltype(f)>
+function_ref(nontype_t<f>)->function_ref<std::remove_pointer_t<decltype(f)>>;
+
+//template<auto f>
+//function_ref(nontype_t<f>, auto)->function_ref<see below>;
+
 
 /*
     template <typename Signature>
--- libraries/core/tests/functional/function_ref.tests.cpp
+++ libraries/core/tests/functional/function_ref.tests.cpp
@@ -5,9 +5,27 @@
 namespace morpheus::functional
 {
 
+TEST_CASE("Propagate constness and noexceptness to function_ref", "[morpheus.functional.function_ref]")
+{
+}
+
+void testFunction() { SUCCEED(); };
+
 TEST_CASE("Verify construction of function_ref", "[morpheus.functional.function_ref]")
 {
-
+    GIVEN("A free standing function")
+    {
+        WHEN("Constructing a function reference to the function")
+        {
+            func_ref_ns::function_ref<void()> functionView = testFunction;
+            THEN("Expect the function to be invocable by the function ref")
+            {
+                functionView();
+            }
+        }
+    }
+//    auto const lambda = [](){};
+//    func_ref_ns::function_ref<void()> lambdaView = lambda;
 }
 
 } // morpheus::functional
