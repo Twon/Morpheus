91e8b1978ec48ff3aae4f2e18ef6cb6c5ab004cf
Antony Peacock <ant.peacock@gmail.com>
2022-11-01 09:18:17 +0000

    Constructors for function_ref

--- libraries/core/src/morpheus/core/functional/function_ref.hpp
+++ libraries/core/src/morpheus/core/functional/function_ref.hpp
@@ -3,6 +3,7 @@
 #include <morpheus/core/conformance/version.hpp>
 #include <morpheus/core/meta/invocable_traits.hpp>
 #include <concepts>
+#include <functional>
 #include <type_traits>
 #include <utility>
 
@@ -38,60 +39,110 @@ template<class Signature> class function_ref;
 ///      [func.wrap.ref.class]
 template<typename Return, class... Args>
 class function_ref<Return(Args...)> {
-public:
-    using signature_type = Return(*)(Args...);
+
     static constexpr bool isNoexcept = meta::IsNothrowInvocable<Return, Args...>;
     static constexpr bool isConst = meta::IsConstInvocable<Return, Args...>;
 
-    template<typename F>
+    template<typename... T>
     static constexpr bool isInvokableWith = []()
     {
         if constexpr (isNoexcept)
-            return std::is_nothrow_invocable_r_v<Return, F, Args...>;
+            return std::is_nothrow_invocable_r_v<Return, T..., Args...>;
         else
-            return std::is_invocable_r_v<Return, F, Args...>;
+            return std::is_invocable_r_v<Return, T..., Args...>;
     }();
+
+    template<typename T>
+    using constness = std::conditional_t<isConst, std::add_const_t<T>, T>;
+
+    template<class F> requires isInvokableWith<F>
+    static constexpr Return invoke(F&& f, Args&& ...args) noexcept(isNoexcept)
+    {
+        if constexpr (std::is_void_v<Return>)
+            std::invoke<F, Args...>(std::forward<F>(f), std::forward<Args>(args)...);
+#if (__cpp_lib_invoke_r >= 202106L)
+        else
+            return std::invoke_r<Return, F, Args...>(std::forward<F>(f), std::forward<Args>(args)...);
+#else
+        else
+            return std::invoke<F, Args...>(std::forward<F>(f), std::forward<Args>(args)...);
+#endif
+    }
+
+    template<class F, class T> requires isInvokableWith<F, constness<T>& >
+    static constexpr Return invoke(F&& f, T&& t, Args&& ...args) noexcept(isNoexcept)
+    {
+        if constexpr (std::is_void_v<Return>)
+            std::invoke<F, T, Args...>(std::forward<F>(f), std::forward<T>(t), std::forward<Args>(args)...);
+#if (__cpp_lib_invoke_r >= 202106L)
+        else
+            return std::invoke_r<Return, F, T, Args...>(std::forward<F>(f), std::forward<T>(t), std::forward<Args>(args)...);
+#else
+        else
+            return std::invoke<F, T, Args...>(std::forward<F>(f), std::forward<T>(t), std::forward<Args>(args)...);
+#endif
+    }
+
+public:
 
     constexpr function_ref() = delete;
 
     template<class F>
-    function_ref(F* f) noexcept requires std::is_function_v<F> and function_ref::isInvokableWith<F>
+    function_ref(F* f) noexcept requires std::is_function_v<F> and isInvokableWith<F>
     :   mInvoke( 
-            [](Storage storage, Args&&... args) noexcept(isNoexcept)
+            [](Storage storage, Args... args) noexcept(isNoexcept)
             {
-                if constexpr (std::is_void_v<Return>)
-                    function_ref::get<F>(storage)(std::forward<Args>(args)...);
-                else
-                    return function_ref::get<F>(storage)(std::forward<Args>(args)...);
+                return invoke(function_ref::get<F>(storage), std::forward<Args>(args)...);
             })
     ,   mStorage(f)
     {}
 
-    template<auto f> constexpr function_ref(nontype_t<f>) noexcept
-    {
+    template<auto F> constexpr function_ref(nontype_t<F>) noexcept
+    :   mInvoke( 
+            [](Storage storage, Args... args) noexcept(isNoexcept)
+            {
+                return invoke(F, std::forward<Args>(args)...);
+            })
+    {}
 
-    }
+    template<auto F, class T>
+    constexpr function_ref(nontype_t<F>, T& object) noexcept requires isInvokableWith<decltype(F), T&>
+    :   mInvoke(
+            [](Storage storage, Args... args) noexcept(isNoexcept) -> Return
+            {
+                auto& object = *function_ref::get<constness<T>>(storage);
+                return invoke(F, object, std::forward<Args>(args)...);
+            })
+    ,   mStorage(std::addressof(object))
+    {}
 
-//    template<auto f, class T> constexpr function_ref(nontype_t<f>, T&) noexcept;
-//    template<auto f, class T> constexpr function_ref(nontype_t<f>, cv T*) noexcept;
+    template<auto F, class T>
+    constexpr function_ref(nontype_t<F>, constness<T> const* object) noexcept requires isInvokableWith<decltype(F), constness<T> const*>
+    :   mInvoke(
+            [](Storage storage, Args... args) noexcept(isNoexcept) -> Return
+            {
+                constness<T>& object = *function_ref::get<T>(storage);
+                return invoke(F, object, std::forward<Args>(args)...);
+            })
+    ,   mStorage(std::addressof(object))
+    {}
 
-    function_ref(function_ref const&) noexcept = default;
+    template<class F, class T = std::remove_reference_t<F>> 
+    constexpr function_ref(F&& f) noexcept 
+        requires (not std::is_same_v<F, function_ref> and not std::is_member_pointer_v<F> and isInvokableWith<F, constness<T>>)
+    :   mInvoke(
+            [](Storage storage, Args... args) noexcept(isNoexcept) -> Return
+            {
+                constness<T>& object = *function_ref::get<T>(storage);
+                return invoke(object, std::forward<Args>(args)...);
+            })
+    ,   mStorage(std::addressof(f))
+    {}
 
-   /*  
+    template <typename F, class T = std::remove_reference_t<F>>
+    function_ref& operator=(F&&) requires (not std::is_same_v<F, function_ref> and not std::is_pointer_v<F>)  = delete;
 
-    template <typename F>
-    function_ref(F&&);
-
-    function_ref& operator=(const function_ref&) noexcept = default;
-
-    template <typename F>
-    function_ref& operator=(F&&);
-
-    void swap(function_ref&) noexcept;
-
-    R operator()(Args...) const noexcept(see below);*/
-
-    constexpr Return operator()(Args&&... args) const noexcept(isNoexcept)
+    constexpr Return operator()(Args... args) const noexcept(isNoexcept)
     {
         return mInvoke(mStorage, std::forward<Args>(args)...);
     }
@@ -103,7 +154,6 @@ public:
     }
 
 private:
-
 
     union Storage
     {
@@ -156,8 +206,6 @@ function_ref(nontype_t<f>)->function_ref<std::remove_pointer_t<decltype(f)>>;
 
 
 /*
-    template <typename Signature>
-    void swap(function_ref<Signature>&, function_ref<Signature>&) noexcept;
 
     template <typename R, typename... Args>
     function_ref(R (*)(Args...)) -> function_ref<R(Args...)>;
--- libraries/core/tests/functional/function_ref.tests.cpp
+++ libraries/core/tests/functional/function_ref.tests.cpp
@@ -7,6 +7,11 @@ namespace morpheus::functional
 
 TEST_CASE("Propagate constness and noexceptness to function_ref", "[morpheus.functional.function_ref]")
 {
+    using ConcreteFunctionRef = func_ref_ns::function_ref<void()>;
+    STATIC_REQUIRE(std::is_nothrow_copy_constructible_v<ConcreteFunctionRef>);
+    STATIC_REQUIRE(std::is_nothrow_copy_assignable_v<ConcreteFunctionRef>);
+    STATIC_REQUIRE(std::is_nothrow_move_constructible_v<ConcreteFunctionRef>);
+    STATIC_REQUIRE(std::is_nothrow_move_assignable_v<ConcreteFunctionRef>);
 }
 
 void testFunction() { SUCCEED(); };
@@ -24,6 +29,42 @@ TEST_CASE("Verify construction of function_ref", "[morpheus.functional.function_
             }
         }
     }
+    GIVEN("A class member function and class instance")
+    {
+        struct TestForInvocable 
+        {
+            bool mRequiredData = true;
+
+            void function(int, int)
+            {
+                SUCCEED();
+            }
+        };
+        WHEN("Constructing a function reference to the function")
+        {
+            TestForInvocable instance;
+            func_ref_ns::function_ref<void(int, int)> functionView = { func_ref_ns::nontype<&TestForInvocable::function>, instance };
+            THEN("Expect the function to be invocable by the function ref")
+            {
+                functionView(0, 1);
+            }   
+        }
+    }
+/*    GIVEN("A class member function and class instance")
+    {
+        auto const lambda = [](int, int){ SUCCEED(); }
+
+        WHEN("Constructing a function reference to the function")
+        {
+            TestForInvocable instance;
+            func_ref_ns::function_ref<void(int, int)> functionView = { func_ref_ns::nontype<&TestForInvocable::function>, instance };
+            THEN("Expect the function to be invocable by the function ref")
+            {
+                functionView(0, 1);
+            }
+        }
+    }
+*/
 //    auto const lambda = [](){};
 //    func_ref_ns::function_ref<void()> lambdaView = lambda;
 }
