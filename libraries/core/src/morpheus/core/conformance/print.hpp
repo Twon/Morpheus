2c95e74a9067a913e3f1232a45a09929d72fc027
Antony Peacock <ant.peacock@gmail.com>
2022-11-04 07:49:27 +0000

    Minor fixes

--- libraries/core/src/morpheus/core/conformance/print.hpp
+++ libraries/core/src/morpheus/core/conformance/print.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #if __has_include(<print>)
-#   include <print>
+#include <print>
+#endif // #if __has_include(<print>)
+
+#if (__cpp_lib_print >= 202207L)
 namespace morpheus { namespace print_ns = std; }
 #else
-#   include <fmt/format.h>
+#   include <fmt/core.h>
 namespace morpheus { namespace print_ns = fmt; }
 #endif
--- libraries/core/tests/meta/is_string.tests.cpp
+++ libraries/core/tests/meta/is_string.tests.cpp
@@ -1,5 +1,5 @@
+#include "morpheus/core/meta/concepts/string.hpp"
 #include "morpheus/core/meta/is_string.hpp"
-
 #include <catch2/catch_all.hpp>
 
 namespace morpheus::meta
@@ -53,6 +53,31 @@ TEST_CASE("Meta is string allows the compile time detection of string view types
     STATIC_REQUIRE(!meta::is_string_view_v<std::uint64_t>);
     STATIC_REQUIRE(!meta::is_string_view_v<float>);
     STATIC_REQUIRE(!meta::is_string_view_v<double>);
+}
+
+TEST_CASE("Concepts to detect string types", "[morpheus.meta.string]")
+{
+    STATIC_REQUIRE(meta::IsString<std::string>);
+    STATIC_REQUIRE(meta::IsString<std::wstring>);
+    STATIC_REQUIRE(meta::IsString<std::u8string>);
+    STATIC_REQUIRE(meta::IsString<std::u16string>);
+    STATIC_REQUIRE(meta::IsString<std::u32string>);
+    STATIC_REQUIRE(!meta::IsString<std::string_view>);
+    STATIC_REQUIRE(!meta::IsString<std::wstring_view>);
+    STATIC_REQUIRE(!meta::IsString<std::u8string_view>);
+    STATIC_REQUIRE(!meta::IsString<std::u16string_view>);
+    STATIC_REQUIRE(!meta::IsString<std::u32string_view>);
+    STATIC_REQUIRE(!meta::IsString<std::int8_t>);
+    STATIC_REQUIRE(!meta::IsString<bool>);
+    STATIC_REQUIRE(!meta::IsString<std::uint8_t>);
+    STATIC_REQUIRE(!meta::IsString<std::int16_t>);
+    STATIC_REQUIRE(!meta::IsString<std::uint16_t>);
+    STATIC_REQUIRE(!meta::IsString<std::int32_t>);
+    STATIC_REQUIRE(!meta::IsString<std::uint32_t>);
+    STATIC_REQUIRE(!meta::IsString<std::int64_t>);
+    STATIC_REQUIRE(!meta::IsString<std::uint64_t>);
+    STATIC_REQUIRE(!meta::IsString<float>);
+    STATIC_REQUIRE(!meta::IsString<double>);
 }
 
 } // namespace morpheus::meta
