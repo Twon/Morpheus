166f235d0d05434e48f19d511c4b2fca9db7947b
Antony Peacock <ant.peacock@gmail.com>
2022-11-24 21:45:35 +0000

    Minor fixes

---
+++ libraries/core/tests/base/verify.tests.cpp
@@ -0,0 +1,12 @@
+#include "morpheus/core/base/verify.hpp"
+
+#include <catch2/catch_all.hpp>
+
+namespace morpheus
+{
+
+TEST_CASE("Ensure assert functionality works and is hooked into by the test framework", "[morpheus.base.verify]")
+{
+}
+
+} // morpheus
