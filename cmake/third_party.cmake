608c7d419648ecf57597c43f35fcbdbf2cb5a855
Antony Peacock <ant.peacock@gmail.com>
2022-12-14 16:29:02 +0000

    Change report directory

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -13,11 +13,11 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/RWTH-HPC/CMake-codecov.git
 )
 
-FetchContent_GetProperties(codecoverage)
-if(NOT codecoverage_POPULATED)
-    FetchContent_Populate(codecoverage)
-    list(APPEND CMAKE_MODULE_PATH ${codecoverage_SOURCE_DIR}/cmake)
-endif()
+#FetchContent_GetProperties(codecoverage)
+#if(NOT codecoverage_POPULATED)
+#    FetchContent_Populate(codecoverage)
+#    list(APPEND CMAKE_MODULE_PATH ${codecoverage_SOURCE_DIR}/cmake)
+#endif()
 
 
 if (NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
@@ -41,11 +41,11 @@ endif()
 )
 FetchContent_MakeAvailable(libunifex)]]#
 
-set(ENABLE_COVERAGE ${MORPHEUS_CODE_COVERAGE} CACHE BOOL "Enable coverage build." FORCE)
-find_package(codecov)
+#set(ENABLE_COVERAGE ${MORPHEUS_CODE_COVERAGE} CACHE BOOL "Enable coverage build." FORCE)
+#find_package(codecov)
 
-if (ENABLE_CODE_COVERAGE)
-    list(APPEND LCOV_REMOVE_PATTERNS "'/usr/*'")
-    list(APPEND LCOV_REMOVE_PATTERNS "'${CMAKE_CURRENT_BINARY_DIR}/*'")
-	set_target_properties(gcov lcov-capture lcov-capture-init lcov-genhtml PROPERTIES FOLDER ${MORPHEUS_PREDEFINED_TARGETS})
-endif()
+#if (ENABLE_CODE_COVERAGE)
+#    list(APPEND LCOV_REMOVE_PATTERNS "'/usr/*'")
+#    list(APPEND LCOV_REMOVE_PATTERNS "'${CMAKE_CURRENT_BINARY_DIR}/*'")
+#	set_target_properties(gcov lcov-capture lcov-capture-init lcov-genhtml PROPERTIES FOLDER ${MORPHEUS_PREDEFINED_TARGETS})
+#endif()
