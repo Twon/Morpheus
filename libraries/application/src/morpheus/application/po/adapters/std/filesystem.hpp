ff43ae4317d25c5a399117fae15232d03de4a799
Antony Peacock <ant.peacock@gmail.com>
2022-12-23 12:03:00 +0000

    Customise lexical_cast for filesytem paths for correct program option handling (#109)

--- libraries/application/src/morpheus/application/po/adapters/std/filesystem.hpp
+++ libraries/application/src/morpheus/application/po/adapters/std/filesystem.hpp
@@ -1,19 +1,24 @@
 #pragma once
 
 #include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <filesystem>
 #include <string>
 #include <vector>
 
-
-namespace morpheus::application::po
+namespace boost
 {
 
-template <class CharType>
-void validate(boost::any& v, std::vector<std::basic_string<CharType>> const& s, std::filesystem::path* p, int)
+/// The default behavour for parsing of std::filesystem::path is fine except for the handling of spaces.  However specialising
+/// boost::lexical_cast for std::filesystem::path so strings are directly convertible to paths is enought to ensure correct
+/// handling of paths
+/// @note
+///     https://github.com/boostorg/program_options/issues/69
+template <>
+inline std::filesystem::path lexical_cast<std::filesystem::path, std::basic_string<char>>(const std::basic_string<char>& arg)
 {
-
+    return std::filesystem::path(arg);
 }
 
-} // namespace morpheus::application::po
+} // namespace boost
--- libraries/application/tests/po/adapters/std/filesystem.tests.cpp
+++ libraries/application/tests/po/adapters/std/filesystem.tests.cpp
@@ -1,12 +1,52 @@
 #include "morpheus/application/po/adapters/std/filesystem.hpp"
-#include <catch2/catch_all.hpp>
+#include "morpheus/application/application.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <boost/dll.hpp>
 
 namespace morpheus::application::po
 {
 
+struct Filesystem
+{
+    std::filesystem::path path;
+
+    void addOptions(boost::program_options::options_description& options)
+    {
+        namespace po = boost::program_options;
+        // clang-format off
+        options.add_options()
+            ("path", po::value(&path), "The location of the path.");
+        // clang-format on
+    }
+};
+
 TEST_CASE("Test parsing of ", "[morpheus.application.po.adapters.std.filesystem]")
 {
-//    REQUIRE();
+    SECTION("Ensure valid value parse correctly")
+    {
+        auto getPath = [](std::string_view param)
+        {
+            Filesystem filesystem{};
+            std::array cliOptions = { "dummyProgram.exe", "--path", param.data() };
+            auto const result = parseProgramOptions(cliOptions.size(), cliOptions.data(), HelpDocumentation{}, filesystem);
+            REQUIRE(!result);
+            return filesystem.path;
+        };
+
+
+        auto const exeLocation = boost::dll::program_location().parent_path();
+        REQUIRE(getPath(".") == std::filesystem::path("."));
+        REQUIRE(getPath("..") == std::filesystem::path(".."));
+        REQUIRE(getPath(exeLocation.string()) == exeLocation);
+        REQUIRE(getPath((exeLocation / ".").lexically_normal().string()) != exeLocation); // Not the same, depends on context: https://stackoverflow.com/questions/42952605/boostfilesystempathlexically-normal-is-this-incorrect-behavior
+        if (exeLocation.has_parent_path())
+            REQUIRE(getPath((exeLocation / "..").lexically_normal().string()) == exeLocation.parent_path());
+
+        // https://github.com/boostorg/program_options/issues/69
+        REQUIRE(getPath("/home/user/path with spaces/.") == std::filesystem::path("/home/user/path with spaces/."));
+    }
+
 }
 
 } // namespace morpheus::application::po
