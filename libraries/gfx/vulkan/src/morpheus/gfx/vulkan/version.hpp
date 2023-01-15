7b8a1eb774de6c1eb664d4b966f65264edbf2995
Antony Peacock <ant.peacock@gmail.com>
2023-01-15 19:20:58 +0000

    Enable vulkan tests

--- libraries/gfx/vulkan/src/morpheus/gfx/vulkan/version.hpp
+++ libraries/gfx/vulkan/src/morpheus/gfx/vulkan/version.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vulkan/vulkan.hpp>
 #include <cstdint>
 
 namespace morpheus::gfx::vulkan
--- libraries/gfx/vulkan/tests/version.tests.cpp
+++ libraries/gfx/vulkan/tests/version.tests.cpp
@@ -11,11 +11,11 @@ TEST_CASE("Test construction of std::error_code object via make_error_code", "[v
     constexpr std::uint8_t expectedMajor = 1;
     constexpr std::uint8_t expectedMinor = 3;
     constexpr std::uint8_t expectedPatch = VK_HEADER_VERSION;
-    Version version(VK_MAKE_API_VERSION(expectedVariant, expectedMajor, expectedMinor, expectedPatch));
-    STATIC_REQUIRES(version.variant());
-    STATIC_REQUIRES(version.major());
-    STATIC_REQUIRES(version.minor());
-    STATIC_REQUIRES(version.patch());
+    constexpr Version version(VK_MAKE_API_VERSION(expectedVariant, expectedMajor, expectedMinor, expectedPatch));
+    STATIC_REQUIRE(version.variant() == expectedVariant);
+    STATIC_REQUIRE(version.major() == expectedMajor);
+    STATIC_REQUIRE(version.minor() == expectedMinor);
+    STATIC_REQUIRE(version.patch() == expectedPatch);
 }
 
 }
