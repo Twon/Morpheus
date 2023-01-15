30325a71ff43a0ed7beb2c2ac5559a92cfcbd506
Antony Peacock <ant.peacock@gmail.com>
2023-01-15 18:56:05 +0000

    Add tests for vulkan vesion

---
+++ libraries/gfx/vulkan/tests/version.tests.cpp
@@ -0,0 +1,21 @@
+#include "morpheus/gfx/vulkan/version.hpp"
+
+#include <catch2/catch_all.hpp>
+
+
+namespace morpheus::gfx::vulkan {
+
+TEST_CASE("Test construction of std::error_code object via make_error_code", "[vulkan.vesion]")
+{
+    constexpr std::uint8_t expectedVariant = 0;
+    constexpr std::uint8_t expectedMajor = 1;
+    constexpr std::uint8_t expectedMinor = 3;
+    constexpr std::uint8_t expectedPatch = VK_HEADER_VERSION;
+    Version version(VK_MAKE_API_VERSION(expectedVariant, expectedMajor, expectedMinor, expectedPatch));
+    STATIC_REQUIRES(version.variant());
+    STATIC_REQUIRES(version.major());
+    STATIC_REQUIRES(version.minor());
+    STATIC_REQUIRES(version.patch());
+}
+
+}
