6370136585cfea73d66183f444164daa87383df1
Antony Peacock <ant.peacock@gmail.com>
2023-04-30 14:12:26 +0100

    Expected serialisation

---
+++ libraries/core/src/morpheus/core/serialisation/adapters/std/expected.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "morpheus/core/conformance/expected.hpp"
+#include "morpheus/core/meta/is_specialisation.hpp"
+#include "morpheus/core/serialisation/concepts/read_serialisable.hpp"
+#include "morpheus/core/serialisation/concepts/read_serialiser.hpp"
+#include "morpheus/core/serialisation/concepts/write_serialisable.hpp"
+#include "morpheus/core/serialisation/concepts/write_serialiser.hpp"
+
+namespace morpheus::serialisation::detail
+{
+
+template <typename T>
+concept IsStdExpected = meta::IsSpecialisationOf<exp_ns::expected, T>;
+
+template <concepts::WriteSerialiser Serialiser, concepts::WriteSerialisable T, concepts::WriteSerialisable E>
+void serialise(Serialiser& serialiser, exp_ns::expected<T, E> const& value)
+{
+    serialiser.writer().beginComposite();
+    serialiser.serialise("state", value.has_value());
+    if (value.has_value()) [[likely]]
+        serialiser.serialise("value", value.value());
+    else
+        serialiser.serialise("error", value.error());
+    serialiser.writer().endComposite();
+}
+
+template <concepts::ReadSerialiser Serialiser, IsStdExpected T>
+T deserialise(Serialiser& serialiser)
+{
+    auto const composite = makeScopedComposite(serialiser.reader());
+    auto const has_value = serialiser.template deserialise<bool>("state");
+    if (has_value)
+        return exp_ns::expected<typename T::value_type, typename T::error_type>(serialiser.template deserialise<typename T::value_type>("value"));
+    else
+        return exp_ns::expected<typename T::value_type, typename T::error_type>(
+            exp_ns::unexpected(serialiser.template deserialise<typename T::error_type>("error")));
+}
+
+} // namespace morpheus::serialisation::detail
