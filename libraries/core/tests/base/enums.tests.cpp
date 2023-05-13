3582a01a5ecd80b7055c12b7434cb5f03b40b11b
Antony Peacock <ant.peacock@gmail.com>
2023-05-13 21:18:59 +0100

    Simple enum tests

--- libraries/core/tests/base/enums.tests.cpp
+++ libraries/core/tests/base/enums.tests.cpp
@@ -21,4 +21,11 @@ TEST_CASE("Ensure magic enum integrates with the Morpheus framework", "[morpheus
     REQUIRE(magic_enum::enum_name(TrafficLights::Green) == "Green");
 }
 
+TEST_CASE("Ensure magic enum converts string to enums", "[morpheus.enum.from_string]")
+{
+    REQUIRE(magic_enum::enum_cast<TrafficLights>("Red") == TrafficLights::Red);
+    REQUIRE(magic_enum::enum_cast<TrafficLights>("Yellow") == TrafficLights::Yellow);
+    REQUIRE(magic_enum::enum_cast<TrafficLights>("Green") == TrafficLights::Green);
+}
+
 } // morpheus
