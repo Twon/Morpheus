a2b290b1b9d56dfa69b18b33029d71eafc946119
Antony Peacock <ant.peacock@gmail.com>
2023-05-28 22:15:49 +0100

    First iteration of an polymorphic value with support for incomplete types

--- libraries/core/src/morpheus/core/memory/polymorphic_value.hpp
+++ libraries/core/src/morpheus/core/memory/polymorphic_value.hpp
@@ -1,13 +1,44 @@
 #pragma once
 
+#include <exception>
 
 namespace morpheus::memory
 {
 
+/// \class bad_polymorphic_value_construction
+///    Objects of type bad_polymorphic_value_construction are thrown to report invalid construction of a polymorphic_value.
+class bad_polymorphic_value_construction : public std::exception
+{
+public:
+    bad_polymorphic_value_construction() noexcept = default;
+
+    /// Ruturns an explanatory string.
+    /// \return Return an implementation defined null terminated byte string.
+    const char* what() const noexcept override;
+};
+
+/// \class polymorphic_value
+///     Implements P0201r6, a polymorphic value type for C++.
+/// \tparam T The underlying polymorphic type.
 template<typename T>
 class polymorphic_value
 {
+public:
+    /// The base type of the underlying polymorphic hierarchy suported as an element in polymorpic value.
+    using element_type = T;
 
+    /// Constructs an empty polymorphic value.
+    constexpr polymorphic_value() noexcept {}
+
+    explicit operator bool() const { return false; }
+
+    constexpr void swap(polymorphic_value& rhs) noexcept
+    {
+        using std::swap;
+        // swap(mValue, rhs.mValue);
+    }
+
+private:
 };
 
 } // namespace morpheus::memory
--- libraries/core/tests/memory/polymorphic_value.tests.cpp
+++ libraries/core/tests/memory/polymorphic_value.tests.cpp
@@ -5,8 +5,53 @@
 namespace morpheus::memory
 {
 
-TEST_CASE("Verify serialisation of polymorphic_value", "[morpheus.memory.polymorphic_value]")
+namespace
 {
+
+struct BaseType
+{
+    virtual int value() const = 0;
+    virtual void set_value(int) = 0;
+    virtual ~BaseType() = default;
+};
+
+struct DerivedType : BaseType
+{
+    int value_ = 0;
+
+    DerivedType() { ++object_count; }
+
+    DerivedType(const DerivedType& d)
+    {
+        value_ = d.value_;
+        ++object_count;
+    }
+
+    DerivedType(int v)
+    : value_(v)
+    {
+        ++object_count;
+    }
+
+    ~DerivedType() { --object_count; }
+
+    int value() const override { return value_; }
+
+    void set_value(int i) override { value_ = i; }
+
+    static size_t object_count;
+};
+
+size_t DerivedType::object_count = 0;
+
+} // namespace
+
+TEST_CASE("Support for incomplete types", "[morpheus.memory.polymorphic_value.class]")
+{
+    class Incomplete;
+    polymorphic_value<Incomplete> p;
+
+    REQUIRE_FALSE(bool(p));
 }
 
 } // morpheus::memory
