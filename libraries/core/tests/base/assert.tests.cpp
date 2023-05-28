194e0c99d607f365a9d15339e819c8dc47e2a98f
Antony Peacock <ant.peacock@gmail.com>
2023-05-28 09:44:51 +0100

    Fixes for asserts and use in testing

--- libraries/core/tests/base/assert.tests.cpp
+++ libraries/core/tests/base/assert.tests.cpp
@@ -1,24 +1,41 @@
-#include <morpheus/core/base/assert.hpp>
 #include <morpheus/catch2/adapters/assert.hpp>
+#include <morpheus/core/base/assert.hpp>
+#include <morpheus/core/base/scoped_action.hpp>
+#include <morpheus/redirect_stream.hpp>
 
 #include <catch2/catch_all.hpp>
+
+#include <iostream>
 
 namespace morpheus
 {
 
 TEST_CASE("Ensure assert functionality works and is hooked into by the test framework", "[morpheus.base.assert]")
 {
-#ifdef MORPHEUS_ASSERT_ENABLED
+#ifdef MORPHEUS_ASSERT_ENABLEDa
     bool fired = false;
-    setAssertHandler([&](Assertion assertion)
+    AssertHandler const currentHandler = getAssetHandler();
+ 
+    auto const onEntry = [&]
     {
-        getPreviousHandler()(assertion);
-        fired = true;
-        return false;
-    });
+        setAssertHandler(
+            [&](Assertion assertion)
+            {
+                getPreviousHandler()(assertion);
+                fired = true;
+                return false;
+            });
+    };
+    
+    auto const onExit = [&](){ setAssertHandler(currentHandler); };
+    ScopedAction const restoreAssertHandler(onEntry, onExit);
+
+    RedirectStream captureOutStream(std::cout);
+    RedirectStream captureErrorStream(std::cerr);
+
     MORPHEUS_ASSERT(false);
     REQUIRE(fired);
 #endif // MORPHEUS_ASSERT_ENABLED
 }
 
-} // morpheus
+} // namespace morpheus
