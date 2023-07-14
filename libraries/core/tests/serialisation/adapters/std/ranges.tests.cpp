1996a40e372ded0868501a02b475c30f519c041e
Antony Peacock <ant.peacock@gmail.com>
2023-07-14 17:40:42 +0100

    Forward list support and further tests

--- libraries/core/tests/serialisation/adapters/std/ranges.tests.cpp
+++ libraries/core/tests/serialisation/adapters/std/ranges.tests.cpp
@@ -1,9 +1,11 @@
 #include "morpheus/core/conformance/ranges.hpp"
 #include "morpheus/core/serialisation/adapters/std/array.hpp"
 #include "morpheus/core/serialisation/adapters/std/deque.hpp"
+#include "morpheus/core/serialisation/adapters/std/forward_list.hpp"
 #include "morpheus/core/serialisation/adapters/std/list.hpp"
 #include "morpheus/core/serialisation/adapters/std/ranges.hpp"
 #include "morpheus/core/serialisation/adapters/std/set.hpp"
+#include "morpheus/core/serialisation/adapters/std/unordered_set.hpp"
 #include "morpheus/core/serialisation/adapters/std/vector.hpp"
 #include "morpheus/core/serialisation/mock/reader.hpp"
 #include "morpheus/core/serialisation/mock/serialisers.hpp"
@@ -22,13 +24,8 @@ namespace morpheus::serialisation
 using namespace ::testing;
 using namespace std::literals::string_view_literals;
 
-
-TEMPLATE_TEST_CASE("Verify serialisation of sequence containers std::ranges", "[morpheus.serialisation.ranges.serialise.sequence_containers]", 
-    (std::array<int, 5>), 
-    std::deque<int>,
-    std::list<int>,
-    std::set<int>,
-    std::vector<int>)
+TEMPLATE_TEST_CASE("Verify serialisation of sequence containers std::ranges", "[morpheus.serialisation.ranges.serialise.sequence_containers]",
+                   (std::array<int, 5>), std::deque<int>, std::list<int>, std::set<int>, std::unordered_set<int>, std::vector<int>)
 {
     GIVEN("A range of values")
     {
@@ -58,4 +55,58 @@ TEMPLATE_TEST_CASE("Verify serialisation of sequence containers std::ranges", "[
     }
 }
 
+/*
+TEST_CASE("Verify deserialisation of std::ranges", "[morpheus.serialisation.ranges.deserialise]")
+{
+    GIVEN("Expected contents of a std::expected holding a value")
+    {
+        constexpr std::int64_t actualValue = 10;
+
+        THEN("Expect the following sequence of operations on the underlying writer")
+        {
+            InSequence seq;
+            MockedReadSerialiser serialiser;
+            EXPECT_CALL(serialiser.reader(), beginComposite()).Times(1);
+            EXPECT_CALL(serialiser.reader(), beginValue("state"sv)).Times(1);
+            EXPECT_CALL(serialiser.reader(), read(An<bool>())).WillOnce(Return(true));
+            EXPECT_CALL(serialiser.reader(), endValue()).Times(1);
+            EXPECT_CALL(serialiser.reader(), beginValue("value"sv)).Times(1);
+            EXPECT_CALL(serialiser.reader(), read(An<std::int64_t>())).WillOnce(Return(actualValue));
+            EXPECT_CALL(serialiser.reader(), endValue()).Times(1);
+            EXPECT_CALL(serialiser.reader(), endComposite()).Times(1);
+            WHEN("Serialising the std::expected")
+            {
+                using ExpectedType = exp_ns::expected<std::int64_t, std::string>;
+                auto const expected = serialiser.deserialise<ExpectedType>();
+                REQUIRE(expected.value() == actualValue);
+            }
+        }
+    }
+    GIVEN("Expected contents of a std::expected holding an error")
+    {
+        std::string const actualValue = "This string is an error";
+
+        THEN("Expect the following sequence of operations on the underlying writer")
+        {
+            InSequence seq;
+            MockedReadSerialiser serialiser;
+            EXPECT_CALL(serialiser.reader(), beginComposite()).Times(1);
+            EXPECT_CALL(serialiser.reader(), beginValue("state"sv)).Times(1);
+            EXPECT_CALL(serialiser.reader(), read(An<bool>())).WillOnce(Return(false));
+            EXPECT_CALL(serialiser.reader(), endValue()).Times(1);
+            EXPECT_CALL(serialiser.reader(), beginValue("error"sv)).Times(1);
+            EXPECT_CALL(serialiser.reader(), read(An<std::string>())).WillOnce(Return(actualValue));
+            EXPECT_CALL(serialiser.reader(), endValue()).Times(1);
+            EXPECT_CALL(serialiser.reader(), endComposite()).Times(1);
+
+            WHEN("Deserialising the std::expected")
+            {
+                using ExpectedType = exp_ns::expected<std::int64_t, std::string>;
+                auto const expected = serialiser.deserialise<ExpectedType>();
+                REQUIRE(expected.error() == actualValue);
+            }
+        }
+    }
+}
+*/
 } // namespace morpheus::serialisation
