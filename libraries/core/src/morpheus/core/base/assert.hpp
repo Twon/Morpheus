9a3e93d1141d547c54d2f691da4bb03057f56dd5
Antony Peacock <ant.peacock@gmail.com>
2023-08-10 11:17:37 +0100

    Tests for verify macro which allow disabling of halting breakpoint

--- libraries/core/src/morpheus/core/base/assert.hpp
+++ libraries/core/src/morpheus/core/base/assert.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <morpheus/core/base/assert.hpp>
 #include <morpheus/core/base/platform.hpp>
+#include <morpheus/core/base/verify.hpp>
 #include <morpheus/core/conformance/source_location.hpp>
 
 #include <cstdint>
@@ -11,45 +13,6 @@
 namespace morpheus
 {
 
-/// \struct Assertion
-///     Minium required information to report an assetion.
-struct Assertion
-{
-    sl_ns::source_location location; ///< The location of the assertion.
-    std::string_view expression;     ///< The expression which resulted in the assertion.
-    std::string message;             ///< Message with human readable decription of the assertion.
-};
-
-/// \typedef AssertHandler
-///     Callback that defines custom behaviour when an assert is hit.
-using AssertHandler = std::function<bool(Assertion)>;
-
-/// Set a custom global assert handler.
-/// \param[in] handler
-///     A custom assert handler to override existing handling behaviour with.
-/// \return
-///     The previous assert handler
-MORPHEUSCORE_EXPORT AssertHandler setAssertHandler(AssertHandler handler);
-
-/// Access the globally enable assert handler.
-[[nodiscard]] MORPHEUSCORE_EXPORT AssertHandler const& getAssertHandler();
-
-/// \enum AssertType
-///     Defines the behavour type for assert handlers.
-enum class MORPHEUSCORE_EXPORT AssertType
-{
-    Assert, ///< Assert behavour calls the handler and calls halt if the handler returns true.
-    Verify  ///< Verify behavour calls the handler and unconditionally calls halt.
-};
-
-/// Triggers the global assert handler.
-/// \param[in] type The type of assert behavour.
-/// \param[in] location The source file location where the assert orginated.
-/// \param[in] expr String representation of the condition.
-/// \param[in] message An optional message describing the assert.
-MORPHEUSCORE_EXPORT void assertHandler(AssertType type, sl_ns::source_location const location,
-                                       std::string_view const expr, std::string_view message = std::string_view());
-
 /// \def MORPHEUS_ASSERT_ENABLED
 ///     Test macro which is enabled by default for debug builds.
 /// \note
@@ -59,15 +22,9 @@ MORPHEUSCORE_EXPORT void assertHandler(AssertType type, sl_ns::source_location c
     #define MORPHEUS_ASSERT_ENABLED
 #endif
 
-#define MORPHEUS_FIRE_ASSERT_HANDLER(type, expr, msg)                                                              \
-    do                                                                                                             \
-        if ((!static_cast<bool>(expr))) [[unlikely]]                                                               \
-            assertHandler(type, MORPHEUS_CURRENT_LOCATION, #expr, msg);                                            \
-    while (0)
-
 
 #if defined(MORPHEUS_ASSERT_ENABLED) 
-    #define MORPHEUS_ASSERT_HANDLER(type, expr, msg)  MORPHEUS_FIRE_ASSERT_HANDLER(type, expr, msg)
+    #define MORPHEUS_ASSERT_HANDLER(type, expr, msg)  MORPHEUS_VERIFY_HANDLER(type, expr, msg)
 #else
     #define MORPHEUS_ASSERT_HANDLER(type, expr, msg)
 #endif
--- libraries/core/src/morpheus/core/base/verify.hpp
+++ libraries/core/src/morpheus/core/base/verify.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
-#include <morpheus/core/base/assert.hpp>
+#include <morpheus/core/base/assert_handler.hpp>
+
+
+#define MORPHEUS_VERIFY_HANDLER(type, expr, msg)                                                                   \
+    do                                                                                                             \
+        if ((!static_cast<bool>(expr))) [[unlikely]]                                                               \
+            assertHandler(type, MORPHEUS_CURRENT_LOCATION, #expr, msg);                                            \
+    while (0)
 
 /// \def MORPHEUS_VERIFY
 ///     Helper macro to verify an expression succeeds.
 /// \param[in] expr
 ///     Expression to verify
-#define MORPHEUS_VERIFY(expr)          MORPHEUS_FIRE_ASSERT_HANDLER(AssertType::Verify, expr, {})
+#define MORPHEUS_VERIFY(expr)          MORPHEUS_VERIFY_HANDLER(AssertType::Verify, expr, {})
 
 /// \def MORPHEUS_VERIFY_MSG
 ///     Helper macro to verify an expression succeeds and print a message on failure.
@@ -14,4 +21,4 @@
 ///     Expression to verify
 /// \param[in] msg
 ///     Message to print on error.
-#define MORPHEUS_VERIFY_MSG(expr, msg) MORPHEUS_FIRE_ASSERT_HANDLER(AssertType::Verify, expr, (msg))
+#define MORPHEUS_VERIFY_MSG(expr, msg) MORPHEUS_VERIFY_HANDLER(AssertType::Verify, expr, (msg))
