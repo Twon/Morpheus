637aa44825324fae52724fae7cc4fbf099687ed5
Antony Peacock <ant.peacock@gmail.com>
2023-08-26 16:03:48 +0100

    Polymorphic value support (#185)
    * First iteration of an polymorphic value with support for incomplete types
    
    * Polymorphic value interface defined
    
    * Further implementation of the polymporphic interface
    
    * Missing concepts
    
    * Style fix
    
    * Remove value method
    
    * Working progress polymorphic value methods
    
    * Add base functionality for polymorphic
    
    * Allocator support for polymorphic
    
    * Gcc fix

---
+++ libraries/core/src/morpheus/core/memory/concepts/deleter.hpp
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include <concepts>
+#include <memory>
+
+namespace morpheus::memory::concepts
+{
+
+/// \concept Deleter
+///     A deleter specified the destruction policity of a given type.
+/// \tparam Callable The callable destruction policy.
+/// \tparam Arg The argument to ne destroyed.
+template <typename Callable, typename Arg>
+concept Deleter = requires(Callable&& c, Arg* a) {
+    {
+        std::forward<Callable>(c)(a)
+    } -> std::same_as<void>;
+};
+
+/// \struct default_copy
+///     Default copier uses default allocation in conjunction with copy assignment operator of T.
+// template <class T>
+// struct default_copy
+// {
+//     /// The deleter type to be used to deallocate objects created by the copier.
+//     using deleter_type = std::default_delete<T>;
+
+//     /// Create a copy of the input.
+//     constexpr T* operator()(T const& t) const { return new T(t); }
+// };
+
+} // namespace morpheus::memory::concepts
---
+++ libraries/core/src/morpheus/core/memory/copier_traits.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <type_traits>
+
+namespace morpheus::memory
+{
+
+template <class T, class = void>
+struct copier_traits_deleter_base
+{};
+
+/// \struct copier_traits_deleter_base<T, std::void_t<typename T::deleter_type>>
+///     Helper specialisation of copier traits which defaults the deleter type to T::deleter_type when present.
+template <class T>
+struct copier_traits_deleter_base<T, std::void_t<typename T::deleter_type>>
+{
+    /// The associated deleter to be used with this copier.
+    using deleter_type = typename T::deleter_type;
+};
+
+/// \struct copier_traits_deleter_base<U* (*)(V)>
+///     Helper specialisation of copier traits which allows defaulting the deleter type with function pointer
+///     equivalent matching signature of the function when used with function pointer copier.
+template <class U, class V>
+struct copier_traits_deleter_base<U* (*)(V)>
+{
+    /// The associated deleter to be used with this copier.
+    using deleter_type = void (*)(U*);
+};
+
+// The user may specialize copier_traits<T> per [namespace.std]/2.
+template <class T>
+struct copier_traits : copier_traits_deleter_base<T, void>
+{};
+
+} // namespace morpheus::memory
