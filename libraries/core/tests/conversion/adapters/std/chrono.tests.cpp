e5b3a3150f097312e00d74b62df4696eb22e2320
Antony Peacock <ant.peacock@gmail.com>
2023-09-01 16:09:34 +0100

    Test valid duration types to print with fmt

--- libraries/core/tests/conversion/adapters/std/chrono.tests.cpp
+++ libraries/core/tests/conversion/adapters/std/chrono.tests.cpp
@@ -12,6 +12,12 @@ namespace morpheus::serialisation
 TEST_CASE("Verify std::format specialisation for std::chrono::duration", "[morpheus.conversion.chrono.duration.formatter]")
 {
     REQUIRE(fmt_ns::format("{}", std::chrono::nanoseconds{45}) == "45ns");
+    REQUIRE(fmt_ns::format("{}", std::chrono::microseconds{378}) == "378us");
+    REQUIRE(fmt_ns::format("{}", std::chrono::milliseconds{2}) == "2ms");
+    REQUIRE(fmt_ns::format("{}", std::chrono::seconds{58}) == "58s");
+    REQUIRE(fmt_ns::format("{}", std::chrono::minutes{42}) == "42m");
+    REQUIRE(fmt_ns::format("{}", std::chrono::hours{5}) == "5h");
+    REQUIRE(fmt_ns::format("{}", std::chrono::days{90}) == "90d");
 }
 
 } // namespace morpheus::serialisation
