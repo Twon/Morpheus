5c66189c73b11054463ff5648bdb7d81a60b40c2
Antony Peacock <ant.peacock@gmail.com>
2023-09-02 11:28:44 +0100

    Handle differences in fmt's handline of chrono for now

--- libraries/core/src/morpheus/core/conversion/adapters/std/chrono.hpp
+++ libraries/core/src/morpheus/core/conversion/adapters/std/chrono.hpp
@@ -13,8 +13,6 @@ struct morpheus::fmt_ns::formatter<morpheus::date_ns::day>
 {
     constexpr auto parse(format_parse_context& ctx) -> format_parse_context::iterator { return std::begin(ctx); }
 
-    // Formats the point p using the parsed format specification (presentation)
-    // stored in this formatter.
     auto format(morpheus::date_ns::day const& day, format_context& ctx) const -> format_context::iterator
     {
         using namespace std::literals;
@@ -27,13 +25,36 @@ struct morpheus::fmt_ns::formatter<morpheus::date_ns::month>
 {
     constexpr auto parse(format_parse_context& ctx) -> format_parse_context::iterator { return std::begin(ctx); }
 
-    // Formats the point p using the parsed format specification (presentation)
-    // stored in this formatter.
     auto format(morpheus::date_ns::month const& month, format_context& ctx) const -> format_context::iterator
     {
         using namespace std::literals;
-        std::array<std::string_view, 12> months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        return morpheus::fmt_ns::format_to(ctx.out(), "{}"sv, months[static_cast<unsigned>(month)]);
+        std::array<std::string_view, 12> constexpr months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        return morpheus::fmt_ns::format_to(ctx.out(), "{}"sv, months[static_cast<unsigned>(month) - 1]);
+    }
+};
+
+template <>
+struct morpheus::fmt_ns::formatter<morpheus::date_ns::year>
+{
+    constexpr auto parse(format_parse_context& ctx) -> format_parse_context::iterator { return std::begin(ctx); }
+
+    auto format(morpheus::date_ns::year const& year, format_context& ctx) const -> format_context::iterator
+    {
+        using namespace std::literals;
+        return morpheus::fmt_ns::format_to(ctx.out(), "{}"sv, static_cast<int>(year));
+    }
+};
+
+template <>
+struct morpheus::fmt_ns::formatter<morpheus::date_ns::weekday>
+{
+    constexpr auto parse(format_parse_context& ctx) -> format_parse_context::iterator { return std::begin(ctx); }
+
+    auto format(morpheus::date_ns::weekday const& day, format_context& ctx) const -> format_context::iterator
+    {
+        using namespace std::literals;
+        std::array<std::string_view, 7> constexpr weekdays = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        return morpheus::fmt_ns::format_to(ctx.out(), "{}"sv, weekdays[day.c_encoding()]);
     }
 };
 
--- libraries/core/tests/conversion/adapters/std/chrono.tests.cpp
+++ libraries/core/tests/conversion/adapters/std/chrono.tests.cpp
@@ -25,7 +25,11 @@ TEST_CASE("Verify std::format specialisation for std::chrono::duration", "[morph
     REQUIRE(fmt_ns::format("{}", std::chrono::minutes{42}) == "42m");
 #endif // (__cpp_lib_format >= 201907L)
     REQUIRE(fmt_ns::format("{}", std::chrono::hours{5}) == "5h");
+#if (__cpp_lib_format >= 201907L)
     REQUIRE(fmt_ns::format("{}", std::chrono::days{90}) == "90d");
+#else
+    REQUIRE(fmt_ns::format("{}", std::chrono::days{90}) == "90[86400]s");
+#endif // (__cpp_lib_format >= 201907L)
 }
 
 TEST_CASE("Verify std::format specialisation for std::chrono::date", "[morpheus.conversion.chrono.date.formatter]")
@@ -33,9 +37,9 @@ TEST_CASE("Verify std::format specialisation for std::chrono::date", "[morpheus.
     using namespace date_ns;
     REQUIRE(fmt_ns::format("{}", day{8}) == "08");
     REQUIRE(fmt_ns::format("{}", month{10}) == "Oct");
-    /*REQUIRE(fmt_ns::format("{}", year{1980}) == "1980");
+    REQUIRE(fmt_ns::format("{}", year{1980}) == "1980");
     REQUIRE(fmt_ns::format("{}", weekday{3}) == "Wed");
-    REQUIRE(fmt_ns::format("{}", weekday_indexed{Wednesday[2]}) == "Wed[2]");
+    /*REQUIRE(fmt_ns::format("{}", weekday_indexed{Wednesday[2]}) == "Wed[2]");
     REQUIRE(fmt_ns::format("{}", weekday_last{Monday[last]}) == "Mon[last]");
     REQUIRE(fmt_ns::format("{}", month_day{month{10}, day{8}}) == "Oct/08");
     REQUIRE(fmt_ns::format("{}", month_day_last{month{10}}) == "Oct/last");
