822ea7d4ac8c483b579a060c51dde66f7742beda
Antony Peacock <ant.peacock@gmail.com>
2023-09-04 17:52:39 +0100

    Gcc compile fix

--- libraries/core/src/morpheus/core/conversion/adapters/std/chrono.hpp
+++ libraries/core/src/morpheus/core/conversion/adapters/std/chrono.hpp
@@ -230,7 +230,7 @@ struct StringConverter<std::chrono::duration<Rep, Period>>
         constexpr auto matchString = []<ctll::fixed_string T>(auto const value) -> exp_ns::expected<std::chrono::duration<Rep, Period>, std::string_view>
         {
             if (auto m = ctre::match<T>(value)) {
-                return std::chrono::duration<Rep, Period>(std::stoi(std::string(m.get<1>().to_view())));
+                return std::chrono::duration<Rep, Period>(std::stoi(std::string(m.template get<1>().to_view())));
             }
             else {
                 return exp_ns::unexpected("Unable to parse std::chrono::duration");
