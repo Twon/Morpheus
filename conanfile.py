f022b14b308ffe79c78ca0c01c05e8c53f053a34
jakeheke75 <jakeheke75@gmail.com>
2023-11-22 22:52:10 +0100

    restore ms-gsl package dependency

--- conanfile.py
+++ conanfile.py
@@ -75,7 +75,7 @@ class Morpheus(ConanFile):
         "glew/2.2.0",
         "gtest/1.13.0",
         "magic_enum/0.8.2",
-        #"ms-gsl/4.0.0",
+        "ms-gsl/4.0.0",
         "rapidjson/cci.20220822",
         "range-v3/0.12.0",
         "tl-expected/20190710",
