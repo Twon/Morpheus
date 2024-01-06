024be4edf62c24b46f2a3851fada9a9e305b168e
Antony Peacock <ant.peacock@gmail.com>
2024-01-06 21:05:46 +0000

    Program option support for std::chrono::timezones (#247)
    * Program option support for std::chrono::timezones
    
    * Use conformance namespace
    
    * Use full scoping
    
    * Update namespace

--- libraries/application/tests/po/adapters/std/chrono.tests.cpp
+++ libraries/application/tests/po/adapters/std/chrono.tests.cpp
@@ -56,4 +56,45 @@ TEST_CASE("Test parsing of std chrono duration", "[morpheus.application.po.adapt
     }
 }
 
+struct TimeZone
+{
+    std::reference_wrapper<date_ns::time_zone const> timezone = *date_ns::get_tzdb().current_zone();
+
+    void addOptions(boost::program_options::options_description& options)
+    {
+        namespace po = boost::program_options;
+        // clang-format off
+        options.add_options()
+            ("timezone", po::value(&timezone), "The timezone.");
+        // clang-format on
+    }
+};
+
+TEST_CASE("Test parsing of std chrono time_zone", "[morpheus.application.po.adapters.std.chrono.time_zone]")
+{
+    SECTION("Ensure valid value parse correctly")
+    {
+        auto getTimezone = [](std::string_view param)
+        {
+            TimeZone timezoneOptions{};
+            std::array cliOptions = {"dummyProgram.exe", "--timezone", param.data()};
+            auto const result = parseProgramOptions(static_cast<int>(cliOptions.size()), cliOptions.data(), HelpDocumentation{}, timezoneOptions);
+            REQUIRE(!result);
+            return timezoneOptions.timezone;
+        };
+
+        REQUIRE(getTimezone("Australia/Sydney") == *date_ns::get_tzdb().locate_zone("Australia/Sydney"));
+        REQUIRE(getTimezone("Brazil/East") == *date_ns::get_tzdb().locate_zone("Brazil/East"));
+        REQUIRE(getTimezone("Canada/Eastern") == *date_ns::get_tzdb().locate_zone("Canada/Eastern"));
+        REQUIRE(getTimezone("US/Central") == *date_ns::get_tzdb().locate_zone("US/Central"));
+    }
+    SECTION("Ensure invalid value parse correctly")
+    {
+        std::array cliOptions = {"dummyProgram.exe", "--timezone", "invalid"};
+        TimeZone timezoneOptions{};
+        auto const result = parseProgramOptions(static_cast<int>(cliOptions.size()), cliOptions.data(), HelpDocumentation{}, timezoneOptions);
+        REQUIRE(result);
+    }
+}
+
 } // namespace morpheus::application::po
