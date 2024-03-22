7fc8ef5f4dc1b99e89984f73dafca5fe49b31059
Antony Peacock <ant.peacock@gmail.com>
2024-03-22 11:24:23 +0900

    Only use version

--- libraries/core/src/morpheus/core/conformance/version.hpp
+++ libraries/core/src/morpheus/core/conformance/version.hpp
@@ -10,8 +10,8 @@
 /// \note
 ///     #include <version> on Windows can accidentally picks up VERSION files.  Use the old <ciso646>
 ///     instead.  For details see: https://bugs.llvm.org/show_bug.cgi?id=42540
-#if __has_include(<version>) && (MORPHEUS_BUILD_PLATFORM == MORPHEUS_TARGET_PLATFORM_PC_WINDOWS)
+//#if __has_include(<version>) && (MORPHEUS_BUILD_PLATFORM == MORPHEUS_TARGET_PLATFORM_PC_WINDOWS)
     #include <version>
-#else
-    #include <ciso646>
-#endif
+//#else
+//    #include <ciso646>
+//#endif
