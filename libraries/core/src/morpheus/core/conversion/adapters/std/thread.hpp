fc17f92620966249f424970cdc598bd3bab1905e
Antony Peacock <ant.peacock@gmail.com>
2024-03-24 10:09:40 +0000

    Provide compiler specific paths

--- libraries/core/src/morpheus/core/conversion/adapters/std/thread.hpp
+++ libraries/core/src/morpheus/core/conversion/adapters/std/thread.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #include "morpheus/core/conformance/format.hpp"
+#include "morpheus/core/conformance/format.hpp"
 
 #include <thread>
-#include <sstream>
 
 // clang-format off
 #if (__cpp_lib_formatters < 202302L)
@@ -15,9 +15,18 @@ struct morpheus::fmt_ns::formatter<std::thread::id, charT>
 
     constexpr auto format(std::thread::id const& id, morpheus::fmt_ns::format_context& ctx) const
     {
-        std::stringstream str;
-        str << id;
-        return morpheus::fmt_ns::format_to(ctx.out(), "{}", str.str());
+#if MORPHEUS_IS_VISUALSTUDIO_COMPATIBLE_COMPILER
+        return morpheus::fmt_ns::format_to(ctx.out(), "{}", id._Get_underlying_id());
+#elif (MORPHEUS_IS_GCC_COMPATIBLE_COMPILER)
+        if constexpr (sizeof(id) == sizeof(std::uint64_t))
+            auto const idAsInt = *reinterpret_cast<std::uint64_t const*>(&id);
+            return morpheus::fmt_ns::format_to(ctx.out(), "{}", idAsInt);
+        else
+            auto const idAsInt = *reinterpret_cast<std::uint32_t const*>(&id);
+            return morpheus::fmt_ns::format_to(ctx.out(), "{}", idAsInt);
+#else
+    #error "Compiler unsupported for fallback std::formatter<std::thread::id> support"
+#endif // (MORPHEUS_COMPILER == MORPHEUS_VISUALSTUDIO_COMPILER)
     }
 };
 
