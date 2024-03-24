9d4a60c095a063c6b518d7b3dac2a91d54d59b05
Antony Peacock <ant.peacock@gmail.com>
2024-03-24 09:08:05 +0000

    std::thread::id formatter adapter

---
+++ libraries/core/src/morpheus/core/conversion/adapters/std/thread.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "morpheus/core/conformance/format.hpp"
+
+#include <thread>
+#include <sstream>
+
+// clang-format off
+#if (__cpp_lib_formatters < 202302L)
+
+template <typename charT>
+struct std::formatter<std::thread:id, charT>
+{
+    constexpr auto parse(morpheus::fmt_ns::format_parse_context& ctx) { return std::begin(ctx); }
+
+    constexpr auto format(std::thread:id const& id, morpheus::fmt_ns::format_context& ctx) const
+    {
+        std::stringstream str;
+        str << id;
+        return morpheus::fmt_ns::format_to(ctx.out(), "{}", str.str());
+    }
+};
+
+#endif
+// clang-format on
---
+++ libraries/core/tests/conformance/thread.tests.cpp
@@ -0,0 +1,33 @@
+#include "morpheus/core/conformance/format.hpp"
+#include "morpheus/core/conversion/adapters/std/thread.hpp"
+#include "morpheus/temp_file.hpp"
+
+#include <catch2/catch_all.hpp>
+#include <thread>
+#include <sstream>
+
+namespace morpheus
+{
+
+TEST_CASE("Ensure std::thread::id format specialisation is supported and working", "[morpheus.conformance.thread]")
+{
+    GIVEN("a capture of the current stack trace")
+    {
+        auto const id = std::this_thread::get_id();
+
+        WHEN("Using std::format to write to a string")
+        {
+            // Ensure time zones work
+            auto const output = fmt_ns::format("{}", id);
+
+            THEN("Expect the variables wrote to the stream in the specified order")
+            {
+                std::stringstream str;
+                str << id;
+                REQUIRE(output == str.str());
+            }
+        }
+    }
+}
+
+}
