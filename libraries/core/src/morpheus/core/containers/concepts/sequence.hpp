903eb4a4ce9f8f2e3626d4a9d382d9412fef9fc8
Antony Peacock <ant.peacock@gmail.com>
2024-06-15 10:35:32 +0100

    Support sequence containers

---
+++ libraries/core/src/morpheus/core/containers/concepts/sequence.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "morpheus/core/containers/concepts/container.hpp"
+
+#include "morpheus/core/conformance/ranges.hpp"
+
+#include <concepts>
+#include <initializer_list>
+#include <version>
+
+namespace morpheus::containers::concepts
+{
+
+/// \concept Sequence
+///     Concept capturing the requirements for a sequence container as outline in the standard at
+///     <a href="https://eel.is/c++draft/sequence.reqmts">[sequence.requirements]</a>, details at
+///     <a href="https://en.cppreference.com/w/cpp/named_req/SequenceContainer">SequenceContainer</a>.
+template <typename T>
+concept Sequence = Container<T> && requires(T t, typename T::value_type v, typename T::size_type s, typename T::iterator i, std::initializer_list<typename T::value_type> il){
+    { T(s, v) };
+    { T(i, i) };
+#if (__cpp_lib_containers_ranges >= 202202L)
+    { T(ranges::from_range, ranges::subrange<typename T::value_type>{}) };
+#endif // (__cpp_lib_containers_ranges >= 202202L)
+    { T(il) };
+    { t = il };
+    { t.insert(i, v) } -> std::same_as<typename T::iterator>;
+    { t.insert(i, std::move(v)) } -> std::same_as<typename T::iterator>;
+    { t.insert(i, s, v) } -> std::same_as<typename T::iterator>;
+    { t.insert(i, i, i) } -> std::same_as<typename T::iterator>;
+#if (__cpp_lib_containers_ranges >= 202202L)
+    { t.insert(i, ranges::subrange<typename T::value_type>{}) } -> std::same_as<typename T::iterator>;
+#endif // (__cpp_lib_containers_ranges >= 202202L)
+    { t.insert(i, il) } -> std::same_as<typename T::iterator>;
+    { t.erase(i) } -> std::same_as<typename T::iterator>;
+    { t.erase(i, i) } -> std::same_as<typename T::iterator>;
+    { t.clear() } -> std::same_as<void>;
+    { t.assign(i, i) } -> std::same_as<void>;
+#if (__cpp_lib_containers_ranges >= 202202L)
+    { t.assign_range(ranges::subrange<typename T::value_type>{}) } -> std::same_as<void>;
+#endif // (__cpp_lib_containers_ranges >= 202202L)
+    { t.assign(il) } -> std::same_as<void>;
+    { t.assign(s, v) } -> std::same_as<void>;
+};
+
+} // namespace morpheus::containers::concepts
---
+++ libraries/core/testing/morpheus/containers/concepts/archtypes/sequence.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "morpheus/containers/concepts/archtypes/container.hpp"
+
+#include <cstddef>
+#include <compare>
+#include <initializer_list>
+#include <iterator>
+
+namespace morpheus::containers::concepts::archtypes
+{
+
+struct Sequence : Container
+{
+    using value_type = int;
+    using reference = value_type&;
+    using const_reference = value_type const&;
+    using iterator = int*;
+    using const_iterator = int const *;
+    using difference_type = std::ptrdiff_t;
+    using size_type = std::size_t;
+
+    constexpr Sequence();
+    constexpr Sequence(size_type, value_type);
+    constexpr Sequence(iterator, iterator);
+#if (__cpp_lib_containers_ranges >= 202202L)
+    constexpr Sequence(ranges::from_range, ranges::subrange<typename T::value_type>{});
+#endif // (__cpp_lib_containers_ranges >= 202202L)
+    constexpr Sequence(std::initializer_list<int>);
+    constexpr Sequence(Sequence const&);
+    constexpr Sequence(Sequence&&);
+    constexpr ~Sequence();
+    constexpr Sequence& operator=(Sequence const&);
+    constexpr Sequence& operator=(Sequence&&);
+    constexpr Sequence& operator=(std::initializer_list<int>);
+
+    constexpr auto operator<=>(Sequence const&) const = default;
+
+    constexpr iterator insert(iterator, value_type);
+    constexpr iterator insert(iterator, size_type, value_type);
+    constexpr iterator insert(iterator, iterator, iterator);
+#if (__cpp_lib_containers_ranges >= 202202L)
+    constexpr iterator insert(iterator, ranges::subrange<value_type>{});
+#endif // (__cpp_lib_containers_ranges >= 202202L)
+    constexpr iterator insert(iterator, std::initializer_list<int>);
+    constexpr iterator erase(iterator);
+    constexpr iterator erase(iterator, iterator);
+    constexpr void clear();
+    constexpr void assign(iterator, iterator);
+#if (__cpp_lib_containers_ranges >= 202202L)
+    constexpr void assign_range(ranges::subrange<value_type>{});
+#endif // (__cpp_lib_containers_ranges >= 202202L)
+    constexpr void assign(std::initializer_list<int>);
+    constexpr void assign(size_type, value_type);
+};
+
+} // namespace morpheus::containers::concepts::archtypes 
