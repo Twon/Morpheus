9399d2164cbf076602b14e84f8ab275656bf60f4
Antony Peacock <ant.peacock@gmail.com>
2024-06-28 19:25:28 +0100

    Add codespell config file

--- libraries/core/tests/containers/concepts/allocator_aware.tests.cpp
+++ libraries/core/tests/containers/concepts/allocator_aware.tests.cpp
@@ -18,7 +18,7 @@ namespace morpheus::containers::concepts
 
 TEMPLATE_TEST_CASE("Verify allocator aware containers concepts", "[morpheus.containers.concepts.allocator_aware]", bool, std::int8_t, std::uint8_t, std::int16_t, std::uint16_t, std::int32_t, std::uint32_t, std::int64_t, std::uint64_t, float, double)
 {
-    SECTION("Archtype test")
+    SECTION("Archetype test")
     {
         STATIC_REQUIRE(AllocatorAware<archetypes::AllocatorAware>);
     }
