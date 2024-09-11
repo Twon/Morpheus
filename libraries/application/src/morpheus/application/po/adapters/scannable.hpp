3d7aeb2752423b40cea8df054c5cb2cd7dd3902f
Antony Peacock <ant.peacock@gmail.com>
2024-09-11 10:32:02 +0100

    Enable scannable types for use with program options

---
+++ libraries/application/src/morpheus/application/po/adapters/scannable.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "morpheus/core/conformance/scan.hpp"
+#include "morpheus/core/meta/concepts/scannable.hpp"
+
+#include <boost/any.hpp>
+#include <boost/program_options.hpp>
+
+#include <string>
+#include <vector>
+
+namespace boost
+{
+
+template <class CharType, typename S>
+requires  morpheus::meta::concepts::Scannable<S, CharType>
+void validate(boost::any& v, std::vector<std::basic_string<CharType>> const& values, S*, int)
+{
+    namespace po = boost::program_options;
+    po::validators::check_first_occurrence(v);
+    auto const& s = po::validators::get_single_string(values);
+
+    auto const result = morpheus::scan_ns::scan<S>(s, "{}");
+    if (result)
+        v = result.value();
+    else
+        throw po::validation_error(po::validation_error::invalid_option_value);
+}
+
+} // namespace boost
