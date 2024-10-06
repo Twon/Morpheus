8fed602bf9930bc9269bd4e4cfc13a518c1f5b06
Antony Peacock <ant.peacock@gmail.com>
2024-10-06 17:19:38 +0100

    Extend is specialisation testing

--- libraries/core/tests/meta/is_specialisation.tests.cpp
+++ libraries/core/tests/meta/is_specialisation.tests.cpp
@@ -2,7 +2,15 @@
 #include "morpheus/core/meta/is_specialisation.hpp"
 
 #include <catch2/catch_all.hpp>
-#include <array>
+
+#include <complex>
+#include <list>
+#include <optional>
+#include <set>
+#include <tuple>
+#include <unordered_set>
+#include <vector>
+
 
 namespace morpheus::meta
 {
@@ -13,20 +21,54 @@ class TestTemplateWith1Param {};
 template <typename T1, typename T2>
 class TestTemplateWith2Param {};
 
+template <typename T>
+inline constexpr bool isVector = meta::IsSpecialisationOf<std::vector, T>;
+
 TEST_CASE("Meta is specialisation allows the compile time detection of specialisations", "[morpheus.meta.is_specialisation]")
 {
-//    STATIC_REQUIRE(meta::is_specialisation_of<std::array, std::array<int, 5>>::value);
-    STATIC_REQUIRE(meta::is_specialisation_of<TestTemplateWith1Param, TestTemplateWith1Param<int>>::value);
-    STATIC_REQUIRE(meta::is_specialisation_of<TestTemplateWith2Param, TestTemplateWith2Param<int,int>>::value);
+    SECTION("Test against simple types")
+    {
+        STATIC_REQUIRE(meta::is_specialisation_of_v<TestTemplateWith1Param, TestTemplateWith1Param<int>>);
+        STATIC_REQUIRE(meta::is_specialisation_of_v<TestTemplateWith2Param, TestTemplateWith2Param<int, int>>);        
+    }
+    SECTION("Test against STL types")
+    {
+        // 1 arg templates
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::complex, std::complex<int>>);
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::list, std::list<int>>);
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::optional, std::optional<int>>);
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::set, std::set<int>>);
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::vector, std::vector<int>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::complex, std::list<int>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::list, std::optional<int>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::optional, std::set<int>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::set, std::vector<int>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::vector, std::complex<int>>);
 
-/*
-    STATIC_REQUIRE(meta::is_specialisation_of<TestTemplateWith1Param<int>>::value);
-    STATIC_REQUIRE(meta::is_specialisation_of<TestTemplateWith2Param<int, int>>::value);
-    STATIC_REQUIRE(meta::is_specialisation_of_v<TestTemplateWith1Param<int>>);
-    STATIC_REQUIRE(meta::is_specialisation_of_v<TestTemplateWith2Param<int, int>>);
-*/
-//    static_assert(meta::is_specialisation_of<std::array<int, 5>>::value);
-//    static_assert(meta::is_specialisation_of_v<std::array<int, 5>>);
+        // 2 arg templates
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::vector, std::vector<int, std::allocator<int>>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::vector, std::list<int, std::allocator<int>>>);
+    
+        // 3 arg templates
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::set, std::set<int, std::less<int>, std::allocator<int>>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::set, std::tuple<int, std::less<int>, std::allocator<int>>>);
+    
+        // 4 arg templates
+        STATIC_REQUIRE(meta::is_specialisation_of_v<std::unordered_set, std::unordered_set<int, std::hash<int>, std::equal_to<int>, std::allocator<int>>>);
+        STATIC_REQUIRE(!meta::is_specialisation_of_v<std::unordered_set, std::tuple<int, std::hash<int>, std::equal_to<int>, std::allocator<int>>>);
+    }
+    SECTION("Create specific meta functions based on higher order is_specialisation_of_v function")
+    {   
+        // Meta functions checking for a Is vector works for different instanstitations of vector
+        STATIC_REQUIRE(isVector<std::vector<int>>);
+        STATIC_REQUIRE(isVector<std::vector<float>>);
+        STATIC_REQUIRE(isVector<std::vector<double>>);
+        STATIC_REQUIRE(isVector<std::vector<int, std::allocator<int>>>);
+
+        // And detects usage with non vector types.
+        STATIC_REQUIRE(!isVector<std::list<int>>);
+        STATIC_REQUIRE(!isVector<std::set<int>>);    
+    }
 }
 
 } // namespace morpheus::meta
