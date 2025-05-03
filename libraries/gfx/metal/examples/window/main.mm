5cb724f4ad88e9abbe10f3a366b7d8982be3530c
Antony Peacock <ant.peacock@gmail.com>
2025-05-03 19:43:40 +0100

    Run pre-commit

--- libraries/gfx/metal/examples/window/main.mm
+++ libraries/gfx/metal/examples/window/main.mm
@@ -46,7 +46,7 @@ int main(int argc, const char * argv[])
                   monitors.startY(),
                   monitors.primary() ? "Primary" : "Secondary");
         }
-        
+
         [NSApplication sharedApplication];
         [NSApp setDelegate: [ApplicationDelegate alloc]];
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
