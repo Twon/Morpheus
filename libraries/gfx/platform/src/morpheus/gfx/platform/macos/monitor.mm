e07676b8707b07a6e68cca27fef01545a65514bc
Antony Peacock <ant.peacock@gmail.com>
2025-05-03 11:18:17 +0100

    Add support for querying monitors on MacOs

---
+++ libraries/gfx/platform/src/morpheus/gfx/platform/macos/monitor.mm
@@ -0,0 +1,32 @@
+
+#include <morpheus/gfx/platform/macos/monitor.h>
+
+#include <CoreGraphics/CoreGraphics.h>
+#include <vector>
+
+namespace morpheus::gfx::macos
+{
+
+concurrency::Generator<Monitor> enumerateMonitors() noexcept
+{
+    uint32_t displayCount = 0;
+    CGGetActiveDisplayList(0, nullptr, &displayCount);
+    std::vector<CGDirectDisplayID> displays(displayCount);
+    CGGetActiveDisplayList(displayCount, displays.data(), &displayCount);
+
+    for (const auto& id : displays) {
+        CGRect bounds = CGDisplayBounds(id);
+        const bool isPrimary = CGDisplayIsMain(id);
+
+        co_yield Monitor{
+            "Display " + std::to_string(id),
+            static_cast<Monitor::Pixels>(bounds.size.width),
+            static_cast<Monitor::Pixels>(bounds.size.height),
+            static_cast<Monitor::PixelDiff>(bounds.origin.x),
+            static_cast<Monitor::PixelDiff>(bounds.origin.y),
+            isPrimary
+        };
+    }
+}
+
+} // namespace morpheus::gfx::macos
---
+++ libraries/gfx/platform/src/morpheus/gfx/platform/monitor.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <compare>
+#include <concepts>
+#include <string>
+#include <string_view>
+#include <type_traits>
+
+namespace morpheus::gfx
+{
+
+/// \class Monitor
+///     Describes an available monitor on the system.
+///
+class Monitor {
+public:
+    using Pixels = std::uint32_t;
+    using PixelDiff = std::int32_t;
+
+    /// \name Life cycle
+    ///@{
+    /// Default construction an empty adapter.
+    constexpr explicit Monitor() noexcept = default;
+
+    /// Constructs an adapter from name and id.
+    constexpr explicit Monitor(
+        std::string_view const name,
+        Pixels const width,
+        Pixels const height,
+        PixelDiff const x,
+        PixelDiff const y,
+        bool const primary = false
+    )   noexcept 
+    :   mName(name)
+    ,   mWidth(width)
+    ,   mHeight(height)
+    ,   mX(x)
+    ,   mY(y)
+    ,   mPrimary(primary)
+    {
+    }
+    ///@}
+
+    /// Access the adapter name.
+    [[nodiscard]] constexpr auto const& name() const noexcept { return mName; }
+
+    /// The width in pixels of the render target.
+    [[nodiscard]] constexpr Pixels width() const noexcept { return mWidth; }
+
+    /// The height in pixels of the render target.
+    [[nodiscard]] constexpr Pixels height() const noexcept { return mHeight; }
+
+    /// Left origin of the monitor in pixels, relative to the global virtual screen coordinate space.
+    [[nodiscard]] constexpr PixelDiff x() const noexcept { return mX; }
+
+    /// Top origin of the monitor in pixels, relative to the global virtual screen coordinate space.
+    [[nodiscard]] constexpr PixelDiff y() const noexcept { return mY; }
+
+    /// Is this the primary monitor?
+    [[nodiscard]] constexpr bool primary() const noexcept { return mPrimary; }
+
+    /// Compare two adapter objects.
+    [[nodiscard]] constexpr auto operator<=>(Monitor const& rhs) const noexcept = default;
+private:
+    /// \name Data Members
+    ///@{
+    std::string mName; //!< The name of the monitor
+    Pixels mWidth = 0; //!< The width in pixels of the screen
+    Pixels mHeight = 0; //!< The height in pixels of the screen
+    PixelDiff mX = 0; //!< Left origin of the monitor in pixels, relative to the global virtual screen coordinate space.
+    PixelDiff mY = 0; //!< Top origin of the monitor in pixels, relative to the global virtual screen coordinate space.
+    bool mPrimary = false; //!< Is this the primary monitor?
+    ///@}
+};
+
+} // namespace morpheus::gfx
