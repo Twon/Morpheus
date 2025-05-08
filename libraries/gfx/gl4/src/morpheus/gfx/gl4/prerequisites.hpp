524addd95e2cdc465c3e2a8702bcef8d26198820
Antony Peacock <ant.peacock@gmail.com>
2025-05-08 18:27:19 +0100

    Rendering layer for win32 (#292)
    * Ensure visualisation has access to render system and add test project
    
    * Test fix is backward compatible with prior versions
    
    * Revert to latest version
    
    * Program options for render system factories
    
    * Run pre-commit
    
    * Keep headers in file sets
    
    * Only expose render system options when the correct requirements are met
    
    * Minor ammendments

--- libraries/gfx/gl4/src/morpheus/gfx/gl4/prerequisites.hpp
+++ libraries/gfx/gl4/src/morpheus/gfx/gl4/prerequisites.hpp
@@ -13,4 +13,6 @@
 #include <OpenGL/gl.h>
 #elif (MORPHEUS_BUILD_PLATFORM == MORPHEUS_TARGET_PLATFORM_LINUX)
 #include <GL/gl.h>
+#else
+    #error "Unsupported platform for GL render system."
 #endif //#if (MORPHEUS_BUILD_PLATFORM==MORPHEUS_TARGET_PLATFORM_PC_WINDOWS)
