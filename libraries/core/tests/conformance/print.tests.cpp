6937b78c5ae87273c7b02b41052ec68fedb60e0c
Antony Peacock <ant.peacock@gmail.com>
2025-07-12 10:47:28 +0100

    Add test to verify formatters work with print

--- libraries/core/tests/conformance/print.tests.cpp
+++ libraries/core/tests/conformance/print.tests.cpp
@@ -1,8 +1,27 @@
+#include "morpheus/core/conformance/format.hpp"
 #include "morpheus/core/conformance/print.hpp"
 #include "morpheus/temp_file.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+
 #include <string>
+
+struct Coordinate
+{
+    int x;
+    int y;
+};
+
+template <>
+struct morpheus::fmt_ns::formatter<Coordinate> : morpheus::fmt_ns::formatter<std::string>
+{
+    template <typename Context>
+    constexpr auto format(Coordinate const& value, Context& context) const
+    {
+        return morpheus::fmt_ns::format_to(context.out(), "{{x={},y={}}}", value.x, value.y);
+    }
+};
+
 
 namespace morpheus
 {
@@ -21,6 +40,27 @@ TEST_CASE("Ensure print is supported and working", "[morpheus.conformance.print]
             THEN("Expect the variables wrote to the stream in the specified order")
             {
                 REQUIRE(temp.contents() == "100 3.14 true");
+            }
+        }
+    }
+}
+
+TEST_CASE("Ensure print is works with formatter customisations", "[morpheus.conformance.print.std_fmt_interop]")
+{
+    GIVEN("A capture to file")
+    {
+        TempFile temp;
+
+        WHEN("Using std::print to write output")
+        {
+            Coordinate coord{50, 100};
+
+            // Ensure time zones work
+            print_ns::print(temp.getFile(), "{}", coord);
+
+            THEN("Expect the variables wrote to the stream in the specified order")
+            {
+                REQUIRE(temp.contents() == "{50, 100}");
             }
         }
     }
