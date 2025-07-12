b66c1beee8f0a7fec526f6f60b35234db1df6814
Antony Peacock <ant.peacock@gmail.com>
2025-07-12 11:24:52 +0100

    Provide fallback for fmt formatter based on std::formatter when std::print is not present but std::format is

--- libraries/core/src/morpheus/core/conformance/print.hpp
+++ libraries/core/src/morpheus/core/conformance/print.hpp
@@ -10,6 +10,30 @@
 #if (__cpp_lib_print >= 202207L)
     namespace morpheus { namespace print_ns = std; }
 #else
+    #if (__cpp_lib_format >= 201907L)
+        #include <fmt/format.h> // IWYU pragma: export
+
+        #include <format> // IWYU pragma: export
+
+        template <typename T, typename CharT>
+        requires std::formattable<T, CharT> && (!fmt::formattable<T, CharT>)
+        struct fmt::formatter<T, CharT>
+        {
+            constexpr auto parse(format_parse_context& ctx)
+            {
+                return ctx.begin();
+            }
+
+            template <typename Context>
+            constexpr auto format(T const& value, Context& context) const
+            {
+                auto const str = std::format("{}", value);
+                return std::format_to(context.out(), "{}", str);
+            }
+        };
+    #endif
+
+
     #include <fmt/core.h> // IWYU pragma: export
     namespace morpheus { namespace print_ns = fmt; }
 #endif
