8dcecc00006fe5e3410e2ae16a3911e38083472b
Antony Peacock <ant.peacock@gmail.com>
2025-07-13 11:27:48 +0100

    Expand exclusion

--- libraries/gfx/platform/tests/x11/render_window.tests.cpp
+++ libraries/gfx/platform/tests/x11/render_window.tests.cpp
@@ -9,10 +9,10 @@ TEST_CASE("Ensure move construction of a x11 render window", "[morpheus.gfx.x11.
 {
     RenderWindow::Config const config;
     RenderWindow::create(config).and_then(
+        // LCOV_EXCL_START
         [&](auto&& window) -> exp_ns::expected<RenderWindow, std::string>
         {
             // The window will not get created on CI as they have no monitors attached so code coverage is disabled.
-            // LCOV_EXCL_START
             REQUIRE(!window.fullScreen());
             REQUIRE(window.width() == config.width);
             REQUIRE(window.height() == config.height);
@@ -24,8 +24,8 @@ TEST_CASE("Ensure move construction of a x11 render window", "[morpheus.gfx.x11.
             REQUIRE(newWindow.height() == config.height);
             REQUIRE(newWindow.colourDepth() == config.colourDepth);
             return exp_ns::expected<RenderWindow, std::string>{std::move(newWindow)};
-            // LCOV_EXCL_STOP
         }
+        // LCOV_EXCL_STOP
     );
 }
 
