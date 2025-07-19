e2007764143eb96f25a7566e2e1da4e7f7333f0f
Antony Peacock <ant.peacock@gmail.com>
2025-07-19 14:30:16 +0100

    Mac os compile fix

--- libraries/gfx/metal/src/morpheus/gfx/metal/render_system.hpp
+++ libraries/gfx/metal/src/morpheus/gfx/metal/render_system.hpp
@@ -10,7 +10,7 @@ namespace morpheus::gfx::metal
  */
 class RenderSystem {
 public:
-    using Window = typename void;
+    using Window = void;
 
     RenderSystem(RenderSystem const&) = delete;
     RenderSystem& operator=(RenderSystem const&) = delete;
