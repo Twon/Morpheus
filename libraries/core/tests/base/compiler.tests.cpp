db6527d84e9e82816d0aca100bfdd87a5f9d1cc8
Antony Peacock <ant.peacock@gmail.com>
2025-07-21 13:10:52 +0100

    Extra test for string view and adapter iterator construction

--- libraries/core/tests/base/compiler.tests.cpp
+++ libraries/core/tests/base/compiler.tests.cpp
@@ -16,11 +16,13 @@ TEST_CASE("Explicitly check compiler feature flags requirements for language fea
 
 TEST_CASE("Explicitly check compiler feature flags requirements for library features", "[morpheus.base.compilers.features.library]")
 {
+    STATIC_REQUIRE(__cpp_lib_adaptor_iterator_pair_constructor >= 202106L); // Iterator pair constructors for std::stack and std::queue.
     STATIC_REQUIRE(__cpp_lib_chrono >= 201611L); // Constexpr support for std::chrono::duration and std::chrono::time_point.
     //STATIC_REQUIRE(__cpp_lib_expected >= 202202L); // std::expected supports monadic operations.
     STATIC_REQUIRE(__cpp_lib_exchange_function >= 201304L); // std::exchange()
     STATIC_REQUIRE(__cpp_lib_filesystem >= 201703L); // std::filesystem library.
     STATIC_REQUIRE(__cpp_lib_optional >= 201606L); // std::optional.
+    STATIC_REQUIRE(__cpp_lib_string_view >= 201606L); // std::string_view.
 }
 
 } // namespace morpheus
