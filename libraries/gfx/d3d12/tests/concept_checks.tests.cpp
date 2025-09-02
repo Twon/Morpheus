db14804dfd74cac4153e2e7feaa568a5018fd581
Antony Peacock <ant.peacock@gmail.com>
2025-09-02 14:50:28 +0100

    Address pre-commit issue

--- libraries/gfx/d3d12/tests/concept_checks.tests.cpp
+++ libraries/gfx/d3d12/tests/concept_checks.tests.cpp
@@ -8,7 +8,7 @@ namespace morpheus::gfx::gl4
 
 TEST_CASE("Test the D3D12 render system adheres to GFX concepts", "[morpheus.gfx.d3d12.concept_checks]")
 {
-    STATIC_REQUIRE(concepts::RenderSystem<RenderSystem>>);
+    STATIC_REQUIRE(concepts::RenderSystem<RenderSystem>);
 }
 
 } // namespace morpheus::gfx::gl4
