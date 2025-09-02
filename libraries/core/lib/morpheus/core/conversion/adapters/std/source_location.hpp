10f7a23418c9f62132f4ff733f340e5c76b9b1b9
Antony Peacock <ant.peacock@gmail.com>
2025-09-02 11:35:44 +0100

    Source location formatting is not constexpr

--- libraries/core/lib/morpheus/core/conversion/adapters/std/source_location.hpp
+++ libraries/core/lib/morpheus/core/conversion/adapters/std/source_location.hpp
@@ -11,7 +11,7 @@ struct morpheus::conf::fmt::formatter<morpheus::conf::sl::source_location>
 {
     constexpr auto parse(format_parse_context& ctx) -> format_parse_context::iterator { return std::begin(ctx); }
 
-    constexpr auto format(morpheus::conf::sl::source_location const& location, format_context& ctx) const -> format_context::iterator
+    auto format(morpheus::conf::sl::source_location const& location, format_context& ctx) const -> format_context::iterator
     {
         using namespace std::literals;
         return morpheus::conf::fmt::format_to(ctx.out(), "{}({}:{}):{}"sv, location.file_name(), location.line(), location.column(), location.function_name());
