25c43f4f11c418db1fefd615b0358ce659ade8f6
Antony Peacock <ant.peacock@gmail.com>
2025-09-04 20:34:45 +0100

    Run pre-commit

--- libraries/gfx/platform/lib/morpheus/gfx/platform/win32/error_codes.hpp
+++ libraries/gfx/platform/lib/morpheus/gfx/platform/win32/error_codes.hpp
@@ -41,7 +41,7 @@ details::ErrorCategory const& ErrorCategory() noexcept;
 /// Overload the global make_error_code() free function with a ErrorCode. It will be found via ADL by the compiler if needed.
 /// \param[in] error The error raised by the underlying Win32 API.
 /// \return A std::error_code with Win32 error encoded into it.
-/// \note Becasue windows error codes are just DWORD values, and thus not strongly typed, you must explicitly wrap them in an ErrorCode constructed from a
+/// \note Because windows error codes are just DWORD values, and thus not strongly typed, you must explicitly wrap them in an ErrorCode constructed from a
 ///       DWORD to pass to the make_error_code overload or call the helper overload getLastErrorCode() which initialised an error code from GetLastError().
 /// \code
 ///     std::error_code ec = make_error_code(morpheus::gfx::win32::ErrorCode{ERROR_FILE_NOT_FOUND});
