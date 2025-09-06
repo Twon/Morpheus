eaea8ad8c4c48d5560c803aa220e04123344b15a
Antony Peacock <ant.peacock@gmail.com>
2025-09-06 21:05:20 +0100

    Use std::out_ptr (#417)
    * Use std::out_ptr
    
    * Correct pointer access

--- libraries/gfx/d3d12/lib/morpheus/gfx/d3d12/types.hpp
+++ libraries/gfx/d3d12/lib/morpheus/gfx/d3d12/types.hpp
@@ -5,10 +5,22 @@
 
 #include <d3d12.h>
 
-#include <wrl/client.h>
+#include <memory>
 
 namespace morpheus::gfx::d3d12
 {
+
+struct ComDeleter
+{
+    template <typename T>
+    void operator()(T* p) const noexcept
+    {
+        if (p)
+        {
+            p->Release();
+        }
+    }
+};
 
 /// \using DXGIExpected
 ///     An alias for an expected which take any value type, but returns an unexpected HRESULT type.
@@ -18,19 +30,19 @@ using DXGIExpected = conf::exp::expected<T, HRESULT>;
 
 /// \using DXGIFactory
 ///     A RAII wrapper for the IDXGIFactory6 interface.
-using DXGIFactory = Microsoft::WRL::ComPtr<IDXGIFactory6>;
+using DXGIFactory = std::unique_ptr<IDXGIFactory6, ComDeleter>;
 
 /// \using DXGIOutput
 ///     A RAII wrapper for the IDXGIOutput interface.
-using DXGIOutput = Microsoft::WRL::ComPtr<IDXGIOutput>;
+using DXGIOutput = std::unique_ptr<IDXGIOutput, ComDeleter>;
 
 /*! \typedef DXGIAdapter
         A RAII wrapper for the IDXGIAdapter1 interface.
  */
-using DXGIAdapter = Microsoft::WRL::ComPtr<IDXGIAdapter1>;
+using DXGIAdapter = std::unique_ptr<IDXGIAdapter1, ComDeleter>;
 
 /// \using D3D12Device
 ///     A RAII wrapper for the ID3D12Device interface.
-using D3D12Device = Microsoft::WRL::ComPtr<ID3D12Device>;
+using D3D12Device = std::unique_ptr<ID3D12Device, ComDeleter>;
 
 } // namespace morpheus::gfx::d3d12
