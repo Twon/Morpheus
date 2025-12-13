33d0366827040ba9627a1e0a2ee1598e09b60427
Antony Peacock <ant.peacock@gmail.com>
2025-12-13 14:37:20 +0000

    Run pre-commit

--- examples/core/network/tcp/blocking/client.cpp
+++ examples/core/network/tcp/blocking/client.cpp
@@ -1,9 +1,9 @@
+#include <common.hpp>
 #include <morpheus/core/conformance/expected.hpp>
 #include <morpheus/core/conformance/print.hpp>
-#include <common.hpp>
 
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #include <expected>
 
@@ -12,45 +12,50 @@ using namespace morpheus::conf;
 
 int main()
 {
-    auto const combineSockAndAddr = [](Socket sock) {
-        return createSockAddr("127.0.0.1", 8080).and_then([sock = std::move(sock)](struct sockaddr_in addr) mutable -> exp::expected<std::pair<Socket, struct sockaddr_in>, std::error_code> {
-            return std::pair{std::move(sock), std::move(addr)};
-        });
+    auto const combineSockAndAddr = [](Socket sock)
+    {
+        return createSockAddr("127.0.0.1", 8080)
+            .and_then([sock = std::move(sock)](struct sockaddr_in addr) mutable -> exp::expected<std::pair<Socket, struct sockaddr_in>, std::error_code>
+                      { return std::pair{std::move(sock), std::move(addr)}; });
     };
 
-    auto const sendMsg = [](SocketAndAddr sockInfo) -> exp::expected<SocketAndAddr, std::error_code> {
+    auto const sendMsg = [](SocketAndAddr sockInfo) -> exp::expected<SocketAndAddr, std::error_code>
+    {
         auto const& [sock, serv_addr] = sockInfo;
-        if (auto const result = sendData(sock, "Hello from the client!")){
+        if (auto const result = sendData(sock, "Hello from the client!"))
+        {
             print::print("Sent {} bytes to server\n", *result);
         }
-        else {
+        else
+        {
             return exp::unexpected(result.error());
         }
         return sockInfo;
     };
 
-    auto const receiveMsg = [](SocketAndAddr sockInfo) -> exp::expected<SocketAndAddr, std::error_code> {
+    auto const receiveMsg = [](SocketAndAddr sockInfo) -> exp::expected<SocketAndAddr, std::error_code>
+    {
         auto const& [sock, serv_addr] = sockInfo;
-        if (auto const result = receiveData(sock)) {
+        if (auto const result = receiveData(sock))
+        {
             return exp::unexpected(result.error());
         }
-        else {
+        else
+        {
             print::print("Received {} bytes from server: {}\n", result->size(), *result);
         }
         return sockInfo;
     };
 
-    auto sockAndAdds = createSocket()
-                           .and_then(combineSockAndAddr)
-                           .and_then(createConnection)
-                           .and_then(sendMsg)
-                           .and_then(receiveMsg);
+    auto sockAndAdds = createSocket().and_then(combineSockAndAddr).and_then(createConnection).and_then(sendMsg).and_then(receiveMsg);
 
-    if (!sockAndAdds) {
+    if (!sockAndAdds)
+    {
         print::print("Error: {}\n", sockAndAdds.error().message());
         return EXIT_FAILURE;
     }
-    else{
+    else
+    {
         return EXIT_SUCCESS;
     }
 }
--- examples/core/network/tcp/blocking/server.cpp
+++ examples/core/network/tcp/blocking/server.cpp
@@ -1,14 +1,15 @@
-#include <morpheus/core/conformance/print.hpp>
 #include <common.hpp>
+#include <morpheus/core/conformance/print.hpp>
 
+#include <arpa/inet.h> // for sockaddr_in, inet_addr()
 #include <cstring>
-#include <unistd.h>         // for close()
-#include <arpa/inet.h>      // for sockaddr_in, inet_addr()
+#include <unistd.h> // for close()
 
 using namespace morpheus;
 using namespace morpheus::conf;
 
-auto createSockAddr(std::uint16_t port) -> conf::exp::expected<struct sockaddr_in, std::error_code> {
+auto createSockAddr(std::uint16_t port) -> conf::exp::expected<struct sockaddr_in, std::error_code>
+{
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces
@@ -16,73 +17,86 @@ auto createSockAddr(std::uint16_t port) -> conf::exp::expected<struct sockaddr_i
     return addr;
 }
 
-auto acceptConnection(SocketAndAddr const& socketInfo) -> exp::expected<Socket, std::error_code> {
+auto acceptConnection(SocketAndAddr const& socketInfo) -> exp::expected<Socket, std::error_code>
+{
     int addrlen = sizeof(socketInfo.second);
     // Accept a client connection
-    if (int client_fd = accept(socketInfo.first.get(), (struct sockaddr *)&socketInfo.second, (socklen_t *)&addrlen); client_fd < 0) {
+    if (int client_fd = accept(socketInfo.first.get(), (struct sockaddr*)&socketInfo.second, (socklen_t*)&addrlen); client_fd < 0)
+    {
         return conf::exp::unexpected(std::error_code(errno, std::system_category()));
     }
-    else{
+    else
+    {
         return Socket(client_fd);
     }
 }
 
-int main() {
+int main()
+{
 
     std::uint16_t const PORT = 8080;
 
-    auto const setSockOptions = [](Socket sock) -> exp::expected<Socket, std::error_code> {
+    auto const setSockOptions = [](Socket sock) -> exp::expected<Socket, std::error_code>
+    {
         // Allow reuse of the address/port
         int opt = 1;
-        if (auto result = setsockopt(sock.get(), SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)); result < 0) {
+        if (auto result = setsockopt(sock.get(), SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)); result < 0)
+        {
             return conf::exp::unexpected(std::error_code(errno, std::system_category()));
         }
         return sock;
     };
 
-    auto const combineSockAndAddr = [&](Socket sock) -> exp::expected<SocketAndAddr, std::error_code> {
-        return createSockAddr(PORT).and_then([sock = std::move(sock)](struct sockaddr_in addr) mutable -> exp::expected<SocketAndAddr, std::error_code> {
-            return std::pair{std::move(sock), std::move(addr)};
-        });
+    auto const combineSockAndAddr = [&](Socket sock) -> exp::expected<SocketAndAddr, std::error_code>
+    {
+        return createSockAddr(PORT).and_then([sock = std::move(sock)](struct sockaddr_in addr) mutable -> exp::expected<SocketAndAddr, std::error_code>
+                                             { return std::pair{std::move(sock), std::move(addr)}; });
     };
 
-    auto const bindAddress = [](SocketAndAddr sockInfo) -> exp::expected<SocketAndAddr, std::error_code> {
+    auto const bindAddress = [](SocketAndAddr sockInfo) -> exp::expected<SocketAndAddr, std::error_code>
+    {
         auto [sock, addr] = std::move(sockInfo);
         // Bind socket to the address
-        if (auto const result = bind(sock.get(), (struct sockaddr *)&addr, sizeof(addr)); result < 0) {
+        if (auto const result = bind(sock.get(), (struct sockaddr*)&addr, sizeof(addr)); result < 0)
+        {
             return conf::exp::unexpected(std::error_code(errno, std::system_category()));
         }
         return std::pair{std::move(sock), std::move(addr)};
     };
 
-    auto const listenOnSocket = [](SocketAndAddr socketInfo) -> exp::expected<SocketAndAddr, std::error_code> {
+    auto const listenOnSocket = [](SocketAndAddr socketInfo) -> exp::expected<SocketAndAddr, std::error_code>
+    {
         // Listen for connections (max backlog 3)
-        if (auto const result = listen(socketInfo.first.get(), 3); result < 0) {
+        if (auto const result = listen(socketInfo.first.get(), 3); result < 0)
+        {
             return conf::exp::unexpected(std::error_code(errno, std::system_category()));
         }
         return socketInfo;
     };
 
-    auto const acceptConnectionAndFwdSocketInfo = [&PORT](SocketAndAddr socketInfo) -> exp::expected<std::tuple<Socket, struct sockaddr_in, Socket>, std::error_code> {
+    auto const acceptConnectionAndFwdSocketInfo =
+        [&PORT](SocketAndAddr socketInfo) -> exp::expected<std::tuple<Socket, struct sockaddr_in, Socket>, std::error_code>
+    {
         print::print("Server listening on port {}...\n", PORT);
-        if (auto client = acceptConnection(socketInfo); !client) {
+        if (auto client = acceptConnection(socketInfo); !client)
+        {
             return exp::unexpected(client.error());
         }
-        else {
+        else
+        {
             return std::tuple{std::move(socketInfo.first), std::move(socketInfo.second), std::move(*client)};
         }
     };
 
-
-    
     auto sockAddrClient = createSocket()
-                           .and_then(setSockOptions)
-                           .and_then(combineSockAndAddr)
-                           .and_then(bindAddress)
-                           .and_then(listenOnSocket)
-                           .and_then(acceptConnectionAndFwdSocketInfo);
+                              .and_then(setSockOptions)
+                              .and_then(combineSockAndAddr)
+                              .and_then(bindAddress)
+                              .and_then(listenOnSocket)
+                              .and_then(acceptConnectionAndFwdSocketInfo);
 
-    if (!sockAddrClient) {
+    if (!sockAddrClient)
+    {
         print::print("Error: {}\n", sockAddrClient.error().message());
         return EXIT_FAILURE;
     }
