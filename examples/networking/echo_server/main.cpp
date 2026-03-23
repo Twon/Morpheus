93cbdf984826e43934bb6ceb1a7cc64b1620cd5d
Antony Peacock <ant.peacock@gmail.com>
2026-03-23 15:56:33 +0000

    Run pre-commit

--- examples/networking/echo_server/main.cpp
+++ examples/networking/echo_server/main.cpp
@@ -1,14 +1,14 @@
-#include <morpheus/application/po/options.hpp>
 #include <morpheus/application/po/adapters/boost/asio.hpp>
+#include <morpheus/application/po/options.hpp>
 
-#include <boost/hana/functional/fix.hpp>
-#include <boost/program_options.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/write.hpp>
+#include <boost/hana/functional/fix.hpp>
+#include <boost/program_options.hpp>
 
 struct Settings
 {
-    //boost::asio::ip::address address;
+    // boost::asio::ip::address address;
     std::uint16_t port = 0;
 
     void addOptions(boost::program_options::options_description& options)
@@ -35,41 +35,42 @@ int main(int argc, char* argv[])
     boost::asio::io_context io_context;
     boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), settings.port));
 
-    auto const doRead = boost::hana::fix([](auto self, std::shared_ptr<boost::asio::ip::tcp::socket> socket) -> void
-    {
-        auto data = std::make_shared<std::array<char, 1024>>();
+    auto const doRead = boost::hana::fix(
+        [](auto self, std::shared_ptr<boost::asio::ip::tcp::socket> socket) -> void
+        {
+            auto data = std::make_shared<std::array<char, 1024>>();
 
-        socket->async_read_some(boost::asio::buffer(data->data(), data->size()),
-            [self, socket, data](boost::system::error_code ec, std::size_t n) mutable -> void
-            {
-                if (!ec)
-                {
-                    boost::asio::async_write(*socket, boost::asio::buffer(data->data(), n), // Write only the 'n' bytes read in.
-                        [self, socket, data](boost::system::error_code ec, std::size_t) mutable
-                        {
-                            if (!ec)
-                            {
-                                self(socket);
-                            }
-                        }
-                    );
-                }
-            }
-        );
-    });
+            socket->async_read_some(boost::asio::buffer(data->data(), data->size()),
+                                    [self, socket, data](boost::system::error_code ec, std::size_t n) mutable -> void
+                                    {
+                                        if (!ec)
+                                        {
+                                            boost::asio::async_write(*socket,
+                                                                     boost::asio::buffer(data->data(), n), // Write only the 'n' bytes read in.
+                                                                     [self, socket, data](boost::system::error_code ec, std::size_t) mutable
+                                                                     {
+                                                                         if (!ec)
+                                                                         {
+                                                                             self(socket);
+                                                                         }
+                                                                     });
+                                        }
+                                    });
+        });
 
-    auto const doAccept = boost::hana::fix([&acceptor, &doRead](auto self) -> void
-    {
-        acceptor.async_accept(
-            [self, &doRead](boost::system::error_code ec, boost::asio::ip::tcp::socket socket)
-            {
-                if (!ec)
+    auto const doAccept = boost::hana::fix(
+        [&acceptor, &doRead](auto self) -> void
+        {
+            acceptor.async_accept(
+                [self, &doRead](boost::system::error_code ec, boost::asio::ip::tcp::socket socket)
                 {
-                    doRead(std::make_shared<boost::asio::ip::tcp::socket>(std::move(socket)));
-                }
-                self();
-            });
-    });
+                    if (!ec)
+                    {
+                        doRead(std::make_shared<boost::asio::ip::tcp::socket>(std::move(socket)));
+                    }
+                    self();
+                });
+        });
 
     doAccept();
 
