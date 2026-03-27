3f761b81fe3836beb66f465b4d66a6699e819234
Antony Peacock <ant.peacock@gmail.com>
2026-03-27 14:07:08 +0000

    Fix warnings

--- libraries/core/tests/meta/invocable_traits.tests.cpp
+++ libraries/core/tests/meta/invocable_traits.tests.cpp
@@ -5,10 +5,15 @@
 namespace morpheus::meta
 {
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunneeded-internal-declaration"
+
 namespace {
 auto exceptInvocableFunction() noexcept(false) {}
 auto noexceptInvocableFunction() noexcept {}
 }
+
+#pragma clang diagnostic pop
 
 TEST_CASE("Meta is nothrow invocable the compile time detection of noexcept invocable types", "[morpheus.meta.is_nothrow_invocable]")
 {
